ec54a63124f3658c8a69626a90ec17bdb02f5274
Nikola Grcevski <grcevski@gmail.com>
2023-04-13 15:24:03 -0400

    Use macros for stipping the debug prints

--- bpf/bpf_dbg.h
+++ bpf/bpf_dbg.h
@@ -1,13 +1,11 @@
 #ifndef BPF_DBG_H
 #define BPF_DBG_H
 
-#ifndef BPF_DEBUG
-#define BPF_DEBUG 0
+#ifdef BPF_DEBUG
+#define bpf_dbg_printk(fmt, args...) bpf_printk(fmt, ##args)
+#else
+#define bpf_dbg_printk(fmt, args...)
 #endif
 
-/* Helper macros to print out debug messages */
-#define bpf_dbg_printk(fmt, args...)                                                               \
-    if (BPF_DEBUG)                                                                                 \
-    bpf_printk(fmt, ##args)
-
 #endif
+

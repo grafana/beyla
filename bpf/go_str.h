6d3564141e22a2998545b271fec121df3a6f0a9e
Nikola Grcevski <grcevski@gmail.com>
2023-04-14 12:22:36 -0400

    Fix null termination bug

--- bpf/go_str.h
+++ bpf/go_str.h
@@ -22,6 +22,12 @@ static __inline int read_go_str(char *name, void *base_ptr, u8 offset, void *fie
         return 0;
     }
 
+    // put in a null terminator if we are not at max_size
+    if (size < max_size) {
+        char null = 0;
+        bpf_probe_read((char *)field + size, 1, &null);
+    }
+
     return 1;
 }
 

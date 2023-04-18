3b3bc00c4ddc931ec45bdffc1b75ccaed1a77830
Nikola Grcevski <grcevski@gmail.com>
2023-04-18 13:41:01 -0400

    Add trace event batching

--- pkg/export/otel/traces.go
+++ pkg/export/otel/traces.go
@@ -74,9 +74,11 @@ func newTracesReporter(svcName, endpoint string) (*TracesReporter, error) {
 		return nil, fmt.Errorf("creating trace exporter: %w", err)
 	}
 
+	bsp := trace.NewBatchSpanProcessor(r.traceExporter, trace.WithMaxExportBatchSize(4096), trace.WithMaxQueueSize(4096))
 	r.traceProvider = trace.NewTracerProvider(
 		trace.WithResource(resources),
-		trace.WithSyncer(r.traceExporter),
+		trace.WithSpanProcessor(bsp),
+		//trace.WithSampler(trace.AlwaysSample()),
 	)
 
 	return &r, nil

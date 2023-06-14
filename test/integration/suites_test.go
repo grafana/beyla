da6f6a67b12d547f86458e8c56987e3064a50bb9
Nikola Grcevski <grcevski@gmail.com>
2023-06-14 19:26:44 -0400

    Move rust ssl at the end

--- test/integration/suites_test.go
+++ test/integration/suites_test.go
@@ -11,17 +11,6 @@ import (
 	"github.com/grafana/ebpf-autoinstrument/test/integration/components/docker"
 )
 
-func TestSuite_RustSSL(t *testing.T) {
-	compose, err := docker.ComposeSuite("docker-compose-rust-tls.yml", path.Join(pathOutput, "test-suite-rust-tls.log"))
-	compose.Env = append(compose.Env, `OPEN_PORT=8490`, `EXECUTABLE_NAME=`)
-	require.NoError(t, err)
-	require.NoError(t, compose.Up())
-	t.Run("Rust RED metrics", testREDMetricsRustHTTPS)
-	t.Run("BPF pinning folder mounted", testBPFPinningMounted)
-	require.NoError(t, compose.Close())
-	t.Run("BPF pinning folder unmounted", testBPFPinningUnmounted)
-}
-
 func TestSuite(t *testing.T) {
 	compose, err := docker.ComposeSuite("docker-compose.yml", path.Join(pathOutput, "test-suite.log"))
 	require.NoError(t, err)
@@ -147,6 +136,17 @@ func TestSuite_Rust(t *testing.T) {
 	require.NoError(t, err)
 	require.NoError(t, compose.Up())
 	t.Run("Rust RED metrics", testREDMetricsRustHTTP)
+	t.Run("BPF pinning folder mounted", testBPFPinningMounted)
+	require.NoError(t, compose.Close())
+	t.Run("BPF pinning folder unmounted", testBPFPinningUnmounted)
+}
+
+func TestSuite_RustSSL(t *testing.T) {
+	compose, err := docker.ComposeSuite("docker-compose-rust-tls.yml", path.Join(pathOutput, "test-suite-rust-tls.log"))
+	compose.Env = append(compose.Env, `OPEN_PORT=8490`, `EXECUTABLE_NAME=`)
+	require.NoError(t, err)
+	require.NoError(t, compose.Up())
+	t.Run("Rust RED metrics", testREDMetricsRustHTTPS)
 	t.Run("BPF pinning folder mounted", testBPFPinningMounted)
 	require.NoError(t, compose.Close())
 	t.Run("BPF pinning folder unmounted", testBPFPinningUnmounted)

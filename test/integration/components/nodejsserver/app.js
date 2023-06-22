91c03974b4b13a505bd98c3ebbe9a20e70e9cc88
Nikola Grcevski <grcevski@gmail.com>
2023-06-22 13:55:12 -0400

    Add NodeJS tests

---
+++ test/integration/components/nodejsserver/app.js
@@ -0,0 +1,15 @@
+var express = require("express");
+var app = express();
+const port = 3030;
+
+app.get("/greeting", (req, res, next) => {
+    res.json("Hello!");
+});
+
+app.get("/smoke", (req, res, next) => {
+    res.sendStatus(200)
+});
+
+app.listen(port, () => {
+    console.log("Server running on port " + port);
+});

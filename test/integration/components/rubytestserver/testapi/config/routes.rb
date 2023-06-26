90b2daa7d6b1f25b8b36c09b2ffa9b3d05b94d40
Nikola Grcevski <grcevski@gmail.com>
2023-06-26 12:51:37 +0200

    Add simple rails app

---
+++ test/integration/components/rubytestserver/testapi/config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  resources :users
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end

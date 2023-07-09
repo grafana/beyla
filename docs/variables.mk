790142f7e998ad46453280412cb0da89c5014ec7
Jack Baldry <jack.baldry@grafana.com>
2023-07-09 09:15:13 +0100

    Add `make docs` procedure for viewing the documentation locally
    Signed-off-by: Jack Baldry <jack.baldry@grafana.com>

---
+++ docs/variables.mk
@@ -0,0 +1,5 @@
+# List of projects to provide to the make-docs script.
+PROJECTS := docs/TODO
+
+# Use alternative image until make-docs 3.0.0 is rolled out everywhere.
+export DOCS_IMAGE := grafana/docs-base:dbd975af06

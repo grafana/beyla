27e5475d6c9f231061f3f324efb15641ab7f9078
Jack Baldry <jack.baldry@grafana.com>
2023-08-17 13:03:54 +0100

    Use latest docs-base image
    Signed-off-by: Jack Baldry <jack.baldry@grafana.com>

--- docs/variables.mk
+++ docs/variables.mk
@@ -4,8 +4,5 @@ PROJECTS := beyla
 # Used to compile Mermaid diagrams as images until this PR is not merged: https://github.com/grafana/website/pull/9196
 export MERMAID_IMAGE := minlag/mermaid-cli:10.2.4
 
-# Use alternative image until make-docs 3.0.0 is rolled out everywhere.
-export DOCS_IMAGE := grafana/docs-base:dbd975af06
-
 # Set the DOC_VALIDATOR_IMAGE to match the one defined in CI.
 export DOC_VALIDATOR_IMAGE := $(shell sed -En 's, *image: "(grafana/doc-validator.*)",\1,p' "$(shell git rev-parse --show-toplevel)/.github/workflows/doc-validator.yml")

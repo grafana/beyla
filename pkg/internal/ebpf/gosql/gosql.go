0bd23b1e15b86a21e4266a403c8e8ffae8f6d029
Matt Frick <matt.frick@grafana.com>
2023-09-18 21:26:14 -0700

    Go SQL: get tracer to install uprobe, print SQL query

---
+++ pkg/internal/ebpf/gosql/gosql.go
@@ -0,0 +1,77 @@
+package gosql
+
+import (
+	"context"
+	"io"
+
+	"github.com/cilium/ebpf"
+	"golang.org/x/exp/slog"
+
+	ebpfcommon "github.com/grafana/beyla/pkg/internal/ebpf/common"
+	"github.com/grafana/beyla/pkg/internal/exec"
+	"github.com/grafana/beyla/pkg/internal/goexec"
+	"github.com/grafana/beyla/pkg/internal/imetrics"
+	"github.com/grafana/beyla/pkg/internal/request"
+)
+
+//go:generate $BPF2GO -cc $BPF_CLANG -cflags $BPF_CFLAGS -target amd64,arm64 bpf ../../../../bpf/go_sql.c -- -I../../../../bpf/headers
+//go:generate $BPF2GO -cc $BPF_CLANG -cflags $BPF_CFLAGS -target amd64,arm64 bpf_debug ../../../../bpf/go_sql.c -- -I../../../../bpf/headers -DBPF_DEBUG
+
+type Tracer struct {
+	Cfg        *ebpfcommon.TracerConfig
+	Metrics    imetrics.Reporter
+	bpfObjects bpfObjects
+	closers    []io.Closer
+}
+
+func (p *Tracer) Load() (*ebpf.CollectionSpec, error) {
+	loader := loadBpf
+	if p.Cfg.BpfDebug {
+		loader = loadBpf_debug
+	}
+	return loader()
+}
+
+func (p *Tracer) Constants(_ *exec.FileInfo, _ *goexec.Offsets) map[string]any {
+	return make(map[string]any)
+}
+
+func (p *Tracer) BpfObjects() any {
+	return &p.bpfObjects
+}
+
+func (p *Tracer) AddCloser(c ...io.Closer) {
+	p.closers = append(p.closers, c...)
+}
+
+func (p *Tracer) GoProbes() map[string]ebpfcommon.FunctionPrograms {
+	return map[string]ebpfcommon.FunctionPrograms{
+		"database/sql.(*DB).queryDC": {
+			Start: p.bpfObjects.UprobeQueryDC,
+			End:   p.bpfObjects.UprobeQueryDC_Returns,
+		},
+	}
+}
+
+func (p *Tracer) KProbes() map[string]ebpfcommon.FunctionPrograms {
+	return nil
+}
+
+func (p *Tracer) UProbes() map[string]map[string]ebpfcommon.FunctionPrograms {
+	return nil
+}
+
+func (p *Tracer) SocketFilters() []*ebpf.Program {
+	return nil
+}
+
+func (p *Tracer) Run(ctx context.Context, eventsChan chan<- []request.Span, svcName string) {
+	logger := slog.With("component", "gosql.Tracer")
+	ebpfcommon.ForwardRingbuf[ebpfcommon.HTTPRequestTrace](
+		svcName,
+		p.Cfg, logger, p.bpfObjects.Events,
+		ebpfcommon.ReadHTTPRequestTraceAsSpan,
+		p.Metrics,
+		append(p.closers, &p.bpfObjects)...,
+	)(ctx, eventsChan)
+}

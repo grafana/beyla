a8cf6da10447ee6802081edd158bb40b14add799
Matt Frick <matt.frick@grafana.com>
2023-10-10 16:52:10 -0700

    Print free storage space after integration test docker compose down

--- test/integration/close_test.go
+++ test/integration/close_test.go
@@ -2,9 +2,11 @@ package integration
 
 import (
 	"os"
+	"os/exec"
 	"testing"
 
 	"github.com/stretchr/testify/require"
+	"golang.org/x/sys/unix"
 )
 
 // prerequisite: the testoutput/run folder was empty before starting the tests
@@ -26,4 +28,24 @@ func testBPFPinningUnmounted(t *testing.T) {
 	entries, err := os.ReadDir(pathVarRun)
 	require.NoError(t, err)
 	require.Empty(t, entries)
+
+	// Convenient hook for monitoring storage space:
+	PrintStorage(t)
+	PrintDockerStorage(t)
+}
+
+func PrintStorage(t *testing.T) {
+	// Convenient hook for monitoring storage space:
+	var stat unix.Statfs_t
+	wd, err := os.Getwd()
+	if err == nil && unix.Statfs(wd, &stat) == nil {
+		t.Logf("Free storage space (in %s) is: %dMB\n", wd, stat.Bavail*uint64(stat.Bsize)/(1024*1024))
+	}
+}
+
+func PrintDockerStorage(t *testing.T) {
+	out, err := exec.Command("docker", "system", "df").CombinedOutput()
+	if err == nil {
+		t.Logf("Docker system df output:\n%s", string(out))
+	}
 }

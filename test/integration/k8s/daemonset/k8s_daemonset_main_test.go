bc520b0877f652f550e2e28be534d74a74fcfdfc
Mario Macias <mario.macias@grafana.com>
2023-10-31 18:48:17 +0100

    K8s DaemonSet integration test (#399)

---
+++ test/integration/k8s/daemonset/k8s_daemonset_main_test.go
@@ -0,0 +1,49 @@
+//go:build integration
+
+package otel
+
+import (
+	"log/slog"
+	"os"
+	"testing"
+	"time"
+
+	"github.com/grafana/beyla/test/integration/components/docker"
+	"github.com/grafana/beyla/test/integration/components/kube"
+	k8s "github.com/grafana/beyla/test/integration/k8s/common"
+	"github.com/grafana/beyla/test/tools"
+)
+
+const (
+	testTimeout = 2 * time.Minute
+
+	jaegerQueryURL = "http://localhost:36686/api/traces"
+)
+
+var cluster *kube.Kind
+
+func TestMain(m *testing.M) {
+	if err := docker.Build(os.Stdout, tools.ProjectDir(),
+		docker.ImageBuild{Tag: "testserver:dev", Dockerfile: k8s.DockerfileTestServer},
+		docker.ImageBuild{Tag: "beyla:dev", Dockerfile: k8s.DockerfileBeyla},
+		docker.ImageBuild{Tag: "grpcpinger:dev", Dockerfile: k8s.DockerfilePinger},
+	); err != nil {
+		slog.Error("can't build docker images", err)
+		os.Exit(-1)
+	}
+
+	cluster = kube.NewKind("test-kind-cluster-otel",
+		kube.ExportLogs(k8s.PathKindLogs),
+		kube.KindConfig(k8s.PathManifests+"/00-kind.yml"),
+		kube.LocalImage("testserver:dev"),
+		kube.LocalImage("beyla:dev"),
+		kube.LocalImage("grpcpinger:dev"),
+		kube.Deploy(k8s.PathManifests+"/01-volumes.yml"),
+		kube.Deploy(k8s.PathManifests+"/03-otelcol.yml"),
+		kube.Deploy(k8s.PathManifests+"/04-jaeger.yml"),
+		kube.Deploy(k8s.PathManifests+"/05-uninstrumented-service.yml"),
+		kube.Deploy(k8s.PathManifests+"/06-beyla-daemonset.yml"),
+	)
+
+	cluster.Run(m)
+}

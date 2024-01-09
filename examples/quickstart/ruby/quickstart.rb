de0bc66f538a0f18b45b0d3ffd32ca06bd98cdfd
Mario Macias <mario.macias@grafana.com>
2024-01-09 15:45:35 +0100

    starting ruby quickstart guide

---
+++ examples/quickstart/ruby/quickstart.rb
@@ -0,0 +1,22 @@
+# Simple web service that just returns Ok to any path.
+require 'socket'
+
+# Initialize a TCPServer object that will listen
+# on localhost:2345 for incoming connections.
+server = TCPServer.new('localhost', 8080)
+
+loop do
+  # Wait until a client connects
+  socket = server.accept
+  request = socket.gets
+  STDERR.puts request
+
+  response = "Hello World!\n"
+  socket.print "HTTP/1.1 200 OK\r\n" +
+                 "Content-Type: text/plain\r\n" +
+                 "Content-Length: #{response.bytesize}\r\n" +
+                 "Connection: close\r\n"
+  socket.print "\r\n"
+  socket.print response
+  socket.close
+end

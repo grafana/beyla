7ccc0b237123795ea4573b5f6fb89cc51b94c85f
Nikola Grcevski <grcevski@gmail.com>
2024-01-30 17:15:12 -0500

    Update offsets

--- configs/offsets/std_inspect.go
+++ configs/offsets/std_inspect.go
@@ -1,10 +1,34 @@
 package main
 
-import "net/http"
+import (
+	"context"
+	"fmt"
+	"net/http"
+)
 
 // This program is used to generate an executable that can be inspected by the go-offsets-tracker tool
 
+func regularGetRequest(ctx context.Context, url string) error {
+	req, err := http.NewRequestWithContext(ctx, http.MethodGet, url, nil)
+	if err != nil {
+		return err
+	}
+
+	rt := http.DefaultTransport
+
+	res, err := rt.RoundTrip(req)
+	if err != nil {
+		return err
+	}
+	defer res.Body.Close()
+
+	fmt.Printf("Status: %v\n", res.Status)
+
+	return nil
+}
+
 func main() {
+	regularGetRequest(context.Background(), "http://localhost:8090/rolldice")
 	http.ListenAndServe(":9090", http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
 		// this doesn't need to have any sense!
 		writer.WriteHeader(request.ProtoMajor)

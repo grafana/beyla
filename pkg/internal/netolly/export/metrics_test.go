6d44a2d4ce8fdc66503473993787c56e5f7bb5cf
Mario Macias <mario.macias@grafana.com>
2024-02-12 16:53:10 +0100

    fix flaky unit test

--- pkg/internal/netolly/export/metrics_test.go
+++ pkg/internal/netolly/export/metrics_test.go
@@ -26,22 +26,25 @@ func TestMetricAttributes(t *testing.T) {
 	}
 	in.Id.SrcIp.In6U.U6Addr8 = [16]uint8{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 255, 255, 12, 34, 56, 78}
 	in.Id.DstIp.In6U.U6Addr8 = [16]uint8{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 255, 255, 33, 22, 11, 1}
-	assert.Equal(t,
-		[]attribute.KeyValue{
-			attribute.String("flow.direction", "egress"),
-			attribute.String("src.address", "12.34.56.78"),
-			attribute.String("server.address", "33.22.11.1"),
-			attribute.Int("server.port", 3210),
-			attribute.String("src.name", "srcname"),
-			attribute.String("src.namespace", "srcnamespace"),
-			attribute.String("dst.name", "dstname"),
-			attribute.String("dst.namespace", "dstnamespace"),
-			attribute.String("asserts.env", "dev"),
-			attribute.String("asserts.site", "dev"),
-			attribute.String("k8s.src.name", "srcname"),
-			attribute.String("k8s.src.namespace", "srcnamespace"),
-			attribute.String("k8s.dst.name", "dstname"),
-			attribute.String("k8s.dst.namespace", "dstnamespace"),
-		},
-		attributes(in))
+
+	reportedAttributes := attributes(in)
+	for _, mustContain := range []attribute.KeyValue{
+		attribute.String("flow.direction", "egress"),
+		attribute.String("src.address", "12.34.56.78"),
+		attribute.String("server.address", "33.22.11.1"),
+		attribute.Int("server.port", 3210),
+		attribute.String("src.name", "srcname"),
+		attribute.String("src.namespace", "srcnamespace"),
+		attribute.String("dst.name", "dstname"),
+		attribute.String("dst.namespace", "dstnamespace"),
+		attribute.String("asserts.env", "dev"),
+		attribute.String("asserts.site", "dev"),
+		attribute.String("k8s.src.name", "srcname"),
+		attribute.String("k8s.src.namespace", "srcnamespace"),
+		attribute.String("k8s.dst.name", "dstname"),
+		attribute.String("k8s.dst.namespace", "dstnamespace"),
+	} {
+		assert.Contains(t, reportedAttributes, mustContain)
+	}
+
 }

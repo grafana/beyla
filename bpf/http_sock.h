aa3c14da1ce88acc5878db09fdc64481ea4ae3db
Nikola Grcevski <grcevski@gmail.com>
2024-04-02 14:58:44 -0400

    Try fail mode

--- bpf/http_sock.h
+++ bpf/http_sock.h
@@ -397,10 +397,13 @@ static __always_inline void process_http2_grpc_frames(pid_connection_info_t *pid
                 saved_buf_pos = pos;
                 if (http_grpc_stream_ended(&frame)) {
                     found_end_frame = 1;
+                    http2_grpc_end(&stream, prev_info, (void *)((u8 *)u_buf + saved_buf_pos));
+
                     break;
                 } 
             } else {
                 found_start_frame = 1;
+                http2_grpc_start(&stream, (void *)((u8 *)u_buf + pos), bytes_len, direction);
                 break;
             }
         }
@@ -425,11 +428,11 @@ static __always_inline void process_http2_grpc_frames(pid_connection_info_t *pid
         }
     }
 
-    if (found_end_frame) {
-        http2_grpc_end(&stream, prev_info, (void *)((u8 *)u_buf + saved_buf_pos));
-    } else if (found_start_frame) {
-        http2_grpc_start(&stream, (void *)((u8 *)u_buf + pos), bytes_len, direction);
-    }
+    // if (found_end_frame) {
+    //     http2_grpc_end(&stream, prev_info, (void *)((u8 *)u_buf + saved_buf_pos));
+    // } else if (found_start_frame) {
+    //     http2_grpc_start(&stream, (void *)((u8 *)u_buf + pos), bytes_len, direction);
+    // }
 
     // We only loop 8 times looking for the stream termination. If the data packed is large we'll miss the
     // frame saying the stream closed. In that case we try this backup path.

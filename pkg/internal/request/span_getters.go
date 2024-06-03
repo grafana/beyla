26ff3d8050ba9e9d083c033c52f1a5a8eb9a7b46
Marc Tuduri <marctc@protonmail.com>
2024-06-03 11:20:18 +0200

    Fix attrs generation

--- pkg/internal/request/span_getters.go
+++ pkg/internal/request/span_getters.go
@@ -40,6 +40,20 @@ func SpanOTELGetters(name attr.Name) (attributes.Getter[*Span, attribute.KeyValu
 		getter = func(s *Span) attribute.KeyValue { return semconv.ServiceName(s.ServiceID.Name) }
 	case attr.DBOperation:
 		getter = func(span *Span) attribute.KeyValue { return DBOperationName(span.Method) }
+	case attr.MessagingSystem:
+		getter = func(span *Span) attribute.KeyValue {
+			if span.Type == EventTypeKafkaClient {
+				return semconv.MessagingSystem("kafka")
+			}
+			return semconv.MessagingSystem("unknown")
+		}
+	case attr.MessagingDestination:
+		getter = func(span *Span) attribute.KeyValue {
+			if span.Type == EventTypeKafkaClient {
+				return semconv.MessagingDestinationName(span.Path)
+			}
+			return semconv.MessagingDestinationName("")
+		}
 	}
 	// default: unlike the Prometheus getters, we don't check here for service name nor k8s metadata
 	// because they are already attributes of the Resource instead of the attributes.

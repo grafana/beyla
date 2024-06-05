2833a13a69eb276e20f66a3dff0200105b054a4c
Mario Macias <mario.macias@grafana.com>
2024-06-05 16:56:55 +0200

    Added extra comment

--- pkg/internal/svc/svc.go
+++ pkg/internal/svc/svc.go
@@ -65,6 +65,9 @@ type ID struct {
 
 	Metadata map[attr.Name]string
 
+	// ProcPID is the PID of the instrumented process as seen by Beyla's /proc filesystem.
+	// It is stored here at process discovery time, because it might differ form the
+	// UserPID and HostPID fields of the request.PidInfo struct.
 	ProcPID int32
 }
 

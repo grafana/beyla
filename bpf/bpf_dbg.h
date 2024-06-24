c3634324accf97ed6c2720f34ace9c7cfdfaca03
Marc Tudurí <marctc@protonmail.com>
2024-06-24 16:50:50 +0200

    Add logger to display BPF logs (#955)

--- bpf/bpf_dbg.h
+++ bpf/bpf_dbg.h
@@ -9,12 +9,38 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
 #ifndef BPF_DBG_H
 #define BPF_DBG_H
 
 #ifdef BPF_DEBUG
-#define bpf_dbg_printk(fmt, args...) bpf_printk(fmt, ##args)
+
+typedef struct log_info {
+    char log[80];
+    char comm[20];
+    u64 pid;
+} log_info_t;
+
+struct {
+    __uint(type, BPF_MAP_TYPE_RINGBUF);
+    __uint(max_entries, 1 << 12);
+    __uint(pinning, LIBBPF_PIN_BY_NAME);    
+} debug_events SEC(".maps");
+
+#define bpf_dbg_helper(fmt, args...) { \
+    {log_info_t *__trace__ = bpf_ringbuf_reserve(&debug_events, sizeof(log_info_t), 0); \
+    if (__trace__) { \
+        BPF_SNPRINTF(__trace__->log, sizeof(__trace__->log), fmt, ##args); \
+        u64 id = bpf_get_current_pid_tgid(); \
+        bpf_get_current_comm(&__trace__->comm, sizeof(__trace__->comm)); \
+        __trace__->pid = id >> 32; \
+        bpf_ringbuf_submit(__trace__, 0); \
+    }} \
+}
+
+#define bpf_dbg_printk(fmt, args...) { \
+    bpf_printk(fmt, ##args); \
+    bpf_dbg_helper(fmt, ##args); \
+}
 #else
 #define bpf_dbg_printk(fmt, args...)
 #endif

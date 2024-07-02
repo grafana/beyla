507bf966af8dc9f8692863f0e177a4bb231eb06f
Nikola Grcevski <grcevski@gmail.com>
2024-07-02 16:16:04 -0400

    more disable

--- test/integration/http2go_test.go
+++ test/integration/http2go_test.go
@@ -152,7 +152,7 @@ func testNestedHTTP2Traces(t *testing.T, url string) {
 }
 
 func TestHTTP2Go(t *testing.T) {
-	t.Skip("seems flaky, we need to look into this")
+	t.Skip("temp disable")
 	compose, err := docker.ComposeSuite("docker-compose-http2.yml", path.Join(pathOutput, "test-suite-http2.log"))
 	// we are going to setup discovery directly in the configuration file
 	compose.Env = append(compose.Env, `BEYLA_EXECUTABLE_NAME=`, `BEYLA_OPEN_PORT=`)

a68f2227d154ef008a5bea40ff64389681217105
Mario Macias <mario.macias@grafana.com>
2024-07-03 10:52:35 +0200

    updating integration test accessory images

--- test/integration/k8s/daemonset/k8s_daemonset_main_test.go
+++ test/integration/k8s/daemonset/k8s_daemonset_main_test.go
@@ -27,9 +27,9 @@ func TestMain(m *testing.M) {
 		docker.ImageBuild{Tag: "testserver:dev", Dockerfile: k8s.DockerfileTestServer},
 		docker.ImageBuild{Tag: "beyla:dev", Dockerfile: k8s.DockerfileBeyla},
 		docker.ImageBuild{Tag: "grpcpinger:dev", Dockerfile: k8s.DockerfilePinger},
-		docker.ImageBuild{Tag: "quay.io/prometheus/prometheus:v2.46.0"},
-		docker.ImageBuild{Tag: "otel/opentelemetry-collector-contrib:0.85.0"},
-		docker.ImageBuild{Tag: "jaegertracing/all-in-one:latest"},
+		docker.ImageBuild{Tag: "quay.io/prometheus/prometheus:v2.53.0"},
+		docker.ImageBuild{Tag: "otel/opentelemetry-collector-contrib:0.104.0"},
+		docker.ImageBuild{Tag: "jaegertracing/all-in-one:1"},
 	); err != nil {
 		slog.Error("can't build docker images", err)
 		os.Exit(-1)
@@ -41,9 +41,9 @@ func TestMain(m *testing.M) {
 		kube.LocalImage("testserver:dev"),
 		kube.LocalImage("beyla:dev"),
 		kube.LocalImage("grpcpinger:dev"),
-		kube.LocalImage("quay.io/prometheus/prometheus:v2.46.0"),
-		kube.LocalImage("otel/opentelemetry-collector-contrib:0.85.0"),
-		kube.LocalImage("jaegertracing/all-in-one:latest"),
+		kube.LocalImage("quay.io/prometheus/prometheus:v2.53.0"),
+		kube.LocalImage("otel/opentelemetry-collector-contrib:0.104.0"),
+		kube.LocalImage("jaegertracing/all-in-one:1"),
 		kube.Deploy(k8s.PathManifests+"/01-volumes.yml"),
 		kube.Deploy(k8s.PathManifests+"/01-serviceaccount.yml"),
 		kube.Deploy(k8s.PathManifests+"/02-prometheus-otelscrape.yml"),

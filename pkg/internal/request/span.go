70c076e48ad30ef220dc6581078ae616b52b6333
Rafael Roquetto <rafael.roquetto@grafana.com>
2024-07-24 10:37:23 -0600

    Support marshalling Span to JSON
    The marshalled JSON object features a different layout in comparison to
    the original Span struct.

--- pkg/internal/request/span.go
+++ pkg/internal/request/span.go
@@ -1,6 +1,9 @@
 package request
 
 import (
+	"encoding/json"
+	"fmt"
+	"strconv"
 	"time"
 	"unicode/utf8"
 
@@ -28,12 +31,58 @@ const (
 	EventTypeKafkaServer
 )
 
+func (t EventType) String() string {
+	switch t {
+	case EventTypeHTTP:
+		return "HTTP"
+	case EventTypeGRPC:
+		return "GRPC"
+	case EventTypeHTTPClient:
+		return "HTTPClient"
+	case EventTypeGRPCClient:
+		return "GRPCClient"
+	case EventTypeSQLClient:
+		return "SQLClient"
+	case EventTypeRedisClient:
+		return "RedisClient"
+	case EventTypeKafkaClient:
+		return "KafkaClient"
+	case EventTypeRedisServer:
+		return "RedisServer"
+	case EventTypeKafkaServer:
+		return "KafkaServer"
+	default:
+		return fmt.Sprintf("UNKNOWN (%d)", t)
+	}
+}
+
+func (t EventType) MarshalText() ([]byte, error) {
+	return []byte(t.String()), nil
+}
+
 type IgnoreMode uint8
 
 const (
 	IgnoreMetrics IgnoreMode = iota + 1
 	IgnoreTraces
 )
+
+func (m IgnoreMode) String() string {
+	switch m {
+	case IgnoreMetrics:
+		return "Metrics"
+	case IgnoreTraces:
+		return "Traces"
+	case 0:
+		return "(none)"
+	default:
+		return fmt.Sprintf("UNKNOWN (%d)", m)
+	}
+}
+
+func (m IgnoreMode) MarshalText() ([]byte, error) {
+	return []byte(m.String()), nil
+}
 
 const (
 	MessagingPublish = "publish"
@@ -63,34 +112,164 @@ type PidInfo struct {
 // REMINDER: any attribute here must be also added to the functions SpanOTELGetters,
 // SpanPromGetters and getDefinitions in pkg/internal/export/metric/definitions.go
 type Span struct {
-	Type           EventType
-	IgnoreSpan     IgnoreMode
-	Method         string
-	Path           string
-	Route          string
-	Peer           string
-	PeerPort       int
-	Host           string
-	HostPort       int
-	Status         int
-	ContentLength  int64
-	RequestStart   int64
-	Start          int64
-	End            int64
-	ServiceID      svc.ID // TODO: rename to Service or ResourceAttrs
-	TraceID        trace2.TraceID
-	SpanID         trace2.SpanID
-	ParentSpanID   trace2.SpanID
-	Flags          uint8
-	Pid            PidInfo
-	PeerName       string
-	HostName       string
-	OtherNamespace string
-	Statement      string
+	Type           EventType      `json:"type"`
+	IgnoreSpan     IgnoreMode     `json:"ignoreSpan"`
+	Method         string         `json:"-"`
+	Path           string         `json:"-"`
+	Route          string         `json:"-"`
+	Peer           string         `json:"peer"`
+	PeerPort       int            `json:"peerPort,string"`
+	Host           string         `json:"host"`
+	HostPort       int            `json:"hostPort,string"`
+	Status         int            `json:"-"`
+	ContentLength  int64          `json:"-"`
+	RequestStart   int64          `json:"-"`
+	Start          int64          `json:"-"`
+	End            int64          `json:"-"`
+	ServiceID      svc.ID         `json:"-"` // TODO: rename to Service or ResourceAttrs
+	TraceID        trace2.TraceID `json:"traceID"`
+	SpanID         trace2.SpanID  `json:"spanID"`
+	ParentSpanID   trace2.SpanID  `json:"parentSpanID"`
+	Flags          uint8          `json:"flags,string"`
+	Pid            PidInfo        `json:"-"`
+	PeerName       string         `json:"peerName"`
+	HostName       string         `json:"hostName"`
+	OtherNamespace string         `json:"-"`
+	Statement      string         `json:"-"`
 }
 
 func (s *Span) Inside(parent *Span) bool {
 	return s.RequestStart >= parent.RequestStart && s.End <= parent.End
+}
+
+const (
+	kClient   = "CLIENT"
+	kServer   = "SERVER"
+	kProducer = "PRODUCER"
+	kConsumer = "CONSUMER"
+	kInternal = "INTERNAL"
+)
+
+func kindString(span *Span) string {
+	switch span.Type {
+	case EventTypeHTTP, EventTypeGRPC, EventTypeKafkaServer, EventTypeRedisServer:
+		return kServer
+	case EventTypeHTTPClient, EventTypeGRPCClient, EventTypeSQLClient, EventTypeRedisClient:
+		return kClient
+	case EventTypeKafkaClient:
+		switch span.Method {
+		case MessagingPublish:
+			return kProducer
+		case MessagingProcess:
+			return kConsumer
+		}
+	}
+	return kInternal
+}
+
+// helper attribute functions used by JSON serialization
+type SpanAttributes map[string]string
+
+func spanAttributes(s *Span) SpanAttributes {
+	switch s.Type {
+	case EventTypeHTTP:
+		return SpanAttributes{
+			"method":     s.Method,
+			"status":     strconv.Itoa(s.Status),
+			"url":        s.Path,
+			"contentLen": strconv.FormatInt(s.ContentLength, 10),
+			"route":      s.Route,
+			"clientAddr": SpanPeer(s),
+			"serverAddr": SpanHost(s),
+			"serverPort": strconv.Itoa(s.HostPort),
+		}
+	case EventTypeHTTPClient:
+		return SpanAttributes{
+			"method":     s.Method,
+			"status":     strconv.Itoa(s.Status),
+			"url":        s.Path,
+			"clientAddr": SpanPeer(s),
+			"serverAddr": SpanHost(s),
+			"serverPort": strconv.Itoa(s.HostPort),
+		}
+	case EventTypeGRPC:
+		return SpanAttributes{
+			"method":     s.Path,
+			"status":     strconv.Itoa(s.Status),
+			"clientAddr": SpanPeer(s),
+			"serverAddr": SpanHost(s),
+			"serverPort": strconv.Itoa(s.HostPort),
+		}
+	case EventTypeGRPCClient:
+		return SpanAttributes{
+			"method":     s.Path,
+			"status":     strconv.Itoa(s.Status),
+			"serverAddr": SpanHost(s),
+			"serverPort": strconv.Itoa(s.HostPort),
+		}
+	case EventTypeSQLClient:
+		return SpanAttributes{
+			"serverAddr": SpanHost(s),
+			"serverPort": strconv.Itoa(s.HostPort),
+			"operation":  s.Method,
+			"table":      s.Path,
+			"statement":  s.Statement,
+		}
+	case EventTypeRedisServer:
+		return SpanAttributes{
+			"serverAddr": SpanHost(s),
+			"serverPort": strconv.Itoa(s.HostPort),
+			"operation":  s.Method,
+			"statement":  s.Statement,
+			"query":      s.Path,
+		}
+	case EventTypeKafkaServer:
+		return SpanAttributes{
+			"serverAddr": SpanHost(s),
+			"serverPort": strconv.Itoa(s.HostPort),
+			"operation":  s.Method,
+			"clientId":   s.OtherNamespace,
+		}
+	}
+
+	return SpanAttributes{}
+}
+
+func (s Span) MarshalJSON() ([]byte, error) {
+	type JSONSpan Span
+
+	t := s.Timings()
+	start := t.RequestStart.UnixMicro()
+	handlerStart := t.Start.UnixMicro()
+	end := t.End.UnixMicro()
+	duration := t.End.Sub(t.RequestStart)
+	handlerDuration := t.End.Sub(t.Start)
+
+	aux := struct {
+		JSONSpan
+		Kind              string         `json:"kind"`
+		Start             int64          `json:"start,string"`
+		HandlerStart      int64          `json:"handlerStart,string"`
+		End               int64          `json:"end,string"`
+		Duration          string         `json:"duration"`
+		DurationUS        int64          `json:"durationUSec,string"`
+		HandlerDuration   string         `json:"handlerDuration"`
+		HandlerDurationUS int64          `json:"handlerDurationUSec,string"`
+		Attributes        SpanAttributes `json:"attributes"`
+	}{
+		JSONSpan:          JSONSpan(s),
+		Kind:              kindString(&s),
+		Start:             start,
+		HandlerStart:      handlerStart,
+		End:               end,
+		Duration:          duration.String(),
+		DurationUS:        duration.Microseconds(),
+		HandlerDuration:   handlerDuration.String(),
+		HandlerDurationUS: handlerDuration.Microseconds(),
+		Attributes:        spanAttributes(&s),
+	}
+
+	return json.Marshal(aux)
 }
 
 type Timings struct {
--- pkg/internal/request/span_test.go
+++ pkg/internal/request/span_test.go
@@ -1,9 +1,12 @@
 package request
 
 import (
+	"encoding/json"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	trace2 "go.opentelemetry.io/otel/trace"
 )
 
 func TestSpanClientServer(t *testing.T) {
@@ -19,5 +22,219 @@ func TestSpanClientServer(t *testing.T) {
 			Type: st,
 		}
 		assert.True(t, span.IsClientSpan())
+	}
+}
+
+func TestEventTypeString(t *testing.T) {
+	typeStringMap := map[EventType]string{
+		EventTypeHTTP:        "HTTP",
+		EventTypeGRPC:        "GRPC",
+		EventTypeHTTPClient:  "HTTPClient",
+		EventTypeGRPCClient:  "GRPCClient",
+		EventTypeSQLClient:   "SQLClient",
+		EventTypeRedisClient: "RedisClient",
+		EventTypeKafkaClient: "KafkaClient",
+		EventTypeRedisServer: "RedisServer",
+		EventTypeKafkaServer: "KafkaServer",
+		EventType(99):        "UNKNOWN (99)",
+	}
+
+	for ev, str := range typeStringMap {
+		assert.Equal(t, ev.String(), str)
+	}
+}
+
+func TestIgnoreModeString(t *testing.T) {
+	modeStringMap := map[IgnoreMode]string{
+		IgnoreMetrics:  "Metrics",
+		IgnoreTraces:   "Traces",
+		IgnoreMode(0):  "(none)",
+		IgnoreMode(99): "UNKNOWN (99)",
+	}
+
+	for mode, str := range modeStringMap {
+		assert.Equal(t, mode.String(), str)
+	}
+}
+
+func TestKindString(t *testing.T) {
+	m := map[*Span]string{
+		&Span{Type: EventTypeHTTP}:                                  kServer,
+		&Span{Type: EventTypeGRPC}:                                  kServer,
+		&Span{Type: EventTypeKafkaServer}:                           kServer,
+		&Span{Type: EventTypeRedisServer}:                           kServer,
+		&Span{Type: EventTypeHTTPClient}:                            kClient,
+		&Span{Type: EventTypeGRPCClient}:                            kClient,
+		&Span{Type: EventTypeSQLClient}:                             kClient,
+		&Span{Type: EventTypeRedisClient}:                           kClient,
+		&Span{Type: EventTypeKafkaClient, Method: MessagingPublish}: kProducer,
+		&Span{Type: EventTypeKafkaClient, Method: MessagingProcess}: kConsumer,
+		&Span{}: kInternal,
+	}
+
+	for span, str := range m {
+		assert.Equal(t, kindString(span), str)
+	}
+}
+
+type jsonObject = map[string]interface{}
+
+func deserializeJSONObject(data []byte) (jsonObject, error) {
+	var object jsonObject
+	err := json.Unmarshal(data, &object)
+
+	return object, err
+}
+
+func TestSerializeJSONSpans(t *testing.T) {
+	type testData struct {
+		eventType EventType
+		attribs   map[string]any
+	}
+
+	tData := []testData{
+		testData{
+			eventType: EventTypeHTTP,
+			attribs: map[string]any{
+				"method":     "method",
+				"status":     "200",
+				"url":        "path",
+				"contentLen": "1024",
+				"route":      "route",
+				"clientAddr": "peername",
+				"serverAddr": "hostname",
+				"serverPort": "5678",
+			},
+		},
+		testData{
+			eventType: EventTypeHTTPClient,
+			attribs: map[string]any{
+				"method":     "method",
+				"status":     "200",
+				"url":        "path",
+				"clientAddr": "peername",
+				"serverAddr": "hostname",
+				"serverPort": "5678",
+			},
+		},
+		testData{
+			eventType: EventTypeGRPC,
+			attribs: map[string]any{
+				"method":     "path",
+				"status":     "200",
+				"clientAddr": "peername",
+				"serverAddr": "hostname",
+				"serverPort": "5678",
+			},
+		},
+		testData{
+			eventType: EventTypeGRPCClient,
+			attribs: map[string]any{
+				"method":     "path",
+				"status":     "200",
+				"serverAddr": "hostname",
+				"serverPort": "5678",
+			},
+		},
+		testData{
+			eventType: EventTypeSQLClient,
+			attribs: map[string]any{
+				"serverAddr": "hostname",
+				"serverPort": "5678",
+				"operation":  "method",
+				"table":      "path",
+				"statement":  "statement",
+			},
+		},
+		testData{
+			eventType: EventTypeRedisClient,
+			attribs:   map[string]any{},
+		},
+		testData{
+			eventType: EventTypeKafkaClient,
+			attribs:   map[string]any{},
+		},
+		testData{
+			eventType: EventTypeRedisServer,
+			attribs: map[string]any{
+				"serverAddr": "hostname",
+				"serverPort": "5678",
+				"operation":  "method",
+				"statement":  "statement",
+				"query":      "path",
+			},
+		},
+		testData{
+			eventType: EventTypeKafkaServer,
+			attribs: map[string]any{
+				"serverAddr": "hostname",
+				"serverPort": "5678",
+				"operation":  "method",
+				"clientId":   "otherns",
+			},
+		},
+	}
+
+	test := func(t *testing.T, tData *testData) {
+		span := Span{
+			Type:           tData.eventType,
+			IgnoreSpan:     IgnoreMetrics,
+			Method:         "method",
+			Path:           "path",
+			Route:          "route",
+			Peer:           "peer",
+			PeerPort:       1234,
+			Host:           "host",
+			HostPort:       5678,
+			Status:         200,
+			ContentLength:  1024,
+			RequestStart:   10000,
+			Start:          15000,
+			End:            35000,
+			TraceID:        trace2.TraceID{0x1, 0x2, 0x3},
+			SpanID:         trace2.SpanID{0x1, 0x2, 0x3},
+			ParentSpanID:   trace2.SpanID{0x1, 0x2, 0x3},
+			Flags:          1,
+			PeerName:       "peername",
+			HostName:       "hostname",
+			OtherNamespace: "otherns",
+			Statement:      "statement",
+		}
+
+		data, err := json.MarshalIndent(span, "", " ")
+
+		require.NoError(t, err)
+
+		s, err := deserializeJSONObject(data)
+
+		require.NoError(t, err)
+
+		assert.Equal(t, map[string]any{
+			"type":                tData.eventType.String(),
+			"kind":                kindString(&span),
+			"ignoreSpan":          "Metrics",
+			"peer":                "peer",
+			"peerPort":            "1234",
+			"host":                "host",
+			"hostPort":            "5678",
+			"peerName":            "peername",
+			"hostName":            "hostname",
+			"start":               s["start"],
+			"handlerStart":        s["handlerStart"],
+			"end":                 s["end"],
+			"duration":            "25µs",
+			"durationUSec":        "25",
+			"handlerDuration":     "20µs",
+			"handlerDurationUSec": "20",
+			"traceID":             "01020300000000000000000000000000",
+			"spanID":              "0102030000000000",
+			"parentSpanID":        "0102030000000000",
+			"flags":               "1",
+			"attributes":          tData.attribs,
+		}, s)
+	}
+
+	for i := range tData {
+		test(t, &tData[i])
 	}
 }

efd9f08a1e645ad7114e79f2ae7639da0ef6afea
Rafael Roquetto <rafael.roquetto@grafana.com>
2024-08-01 12:36:38 -0600

    Fix privileged test

--- pkg/internal/helpers/capabilities_linux_privilieged_test.go
+++ pkg/internal/helpers/capabilities_linux_privilieged_test.go
@@ -12,6 +12,7 @@ import (
 const privilegedEnv = "PRIVILEGED_TESTS"
 
 var expectedProcCaps *OSCapabilities
+var errResetCaps error
 
 // This needs to run in the main thread (called by TestMain() below)
 // capset() can fail with EPERM when called from a different thread. From the
@@ -23,36 +24,47 @@ var expectedProcCaps *OSCapabilities
 //	For  kernels  lacking  VFS  support,  the CAP_SETPCAP  capability  is  required.
 //
 // We need to drop capabilities to correctly test TestCheckOSCapabilities()
-func resetProcCapabilities() error {
+func resetProcCapabilities() {
 	var err error
 
 	expectedProcCaps, err = GetCurrentProcCapabilities()
 
+	errRef := &err
+	cleanup := func() {
+		if *errRef != nil {
+			errResetCaps = fmt.Errorf("failed to reset capabilities: %w", *errRef)
+		}
+	}
+
+	defer cleanup()
+
 	if err != nil {
-		return err
+		return
 	}
 
 	expectedProcCaps.Clear(unix.CAP_BPF)
 	expectedProcCaps.Set(unix.CAP_BPF)
 
-	return SetCurrentProcCapabilities(expectedProcCaps)
+	err = SetCurrentProcCapabilities(expectedProcCaps)
 }
 
 func TestGetSetCurrentProcCaps(t *testing.T) {
+	if os.Getenv(privilegedEnv) == "" {
+		t.Skipf("Set %s to run this test\n", privilegedEnv)
+	}
+
+	if errResetCaps != nil {
+		assert.Fail(t, errResetCaps.Error())
+	}
+
 	caps, err := GetCurrentProcCapabilities()
 	assert.NoError(t, err)
 	assert.Equal(t, expectedProcCaps, caps)
 }
 
 func TestMain(m *testing.M) {
-	if os.Getenv(privilegedEnv) == "" {
-		fmt.Printf("Set %s to run this test\n", privilegedEnv)
-		os.Exit(0)
-	}
-
-	if err := resetProcCapabilities(); err != nil {
-		fmt.Printf("Failed to reset capabilities: %s\n", err)
-		os.Exit(-1)
+	if os.Getenv(privilegedEnv) != "" {
+		resetProcCapabilities()
 	}
 
 	os.Exit(m.Run())

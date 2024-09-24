01491bf124cfd55ae908bea0a38e41bdeaddb269
Marc Tuduri <marctc@protonmail.com>
2024-09-24 11:45:52 +0200

    Revert "refactor go_common.h - fix find_parent_goroutine"
    This reverts commit cf6843c648e35c154d1979d8644fcdc57bb85865.

--- bpf/go_common.h
+++ bpf/go_common.h
@@ -88,12 +88,10 @@ static __always_inline u64 find_parent_goroutine(goroutine_key_t *current) {
 
     u64 r_addr = current->addr;
     goroutine_key_t *parent = current;
-    goroutine_key_t g_key = {};
 
     int attempts = 0;
     do {
-        goroutine_key_from_id(&g_key, (void *)r_addr);
-        tp_info_t *p_inv = bpf_map_lookup_elem(&go_trace_map, &g_key);
+        tp_info_t *p_inv = bpf_map_lookup_elem(&go_trace_map, parent);
         if (!p_inv) { // not this goroutine running the server request processing
             // Let's find the parent scope
             goroutine_metadata *g_metadata =

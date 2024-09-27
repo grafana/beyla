cd6081aab4bb56121701fdfb0656b3c3addce993
Nikola Grcevski <grcevski@gmail.com>
2024-09-27 11:41:13 -0400

    fix and unit test env parsing

--- pkg/internal/exec/procenv.go
+++ pkg/internal/exec/procenv.go
@@ -1,11 +1,28 @@
 package exec
 
 import (
-	"fmt"
 	"strings"
 
 	"github.com/prometheus/procfs"
 )
+
+func envStrsToMap(varsStr []string) map[string]string {
+	vars := make(map[string]string, len(varsStr))
+
+	for _, s := range varsStr {
+		parts := strings.Split(s, "=")
+		if len(parts) >= 2 {
+			key := strings.TrimSpace(parts[0])
+			val := strings.TrimSpace(strings.Join(parts[1:], "="))
+
+			if key != "" && val != "" {
+				vars[key] = val
+			}
+		}
+	}
+
+	return vars
+}
 
 func EnvVars(pid int32) (map[string]string, error) {
 	proc, err := procfs.NewProc(int(pid))
@@ -20,16 +37,5 @@ func EnvVars(pid int32) (map[string]string, error) {
 		return nil, err
 	}
 
-	vars := make(map[string]string, len(varsStr))
-
-	for _, s := range varsStr {
-		parts := strings.Split(s, "=")
-		if len(parts) == 2 {
-			vars[parts[0]] = parts[1]
-		}
-	}
-
-	fmt.Printf("%v\n", vars)
-
-	return vars, nil
+	return envStrsToMap(varsStr), nil
 }

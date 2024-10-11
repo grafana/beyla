ac3b1377135a72b2ba54c0a5c470360af94413f1
Nikola Grcevski <6207777+grcevski@users.noreply.github.com>
2024-10-11 19:57:13 -0400

    Fix edge condition with kafka request parsing (#1252)

--- pkg/internal/ebpf/common/kafka_detect_transform_test.go
+++ pkg/internal/ebpf/common/kafka_detect_transform_test.go
@@ -14,6 +14,15 @@ func TestProcessKafkaRequest(t *testing.T) {
 		expected *KafkaInfo
 	}{
 		{
+			name:  "Fetch request (v11) truncated - 1",
+			input: []byte{0, 0, 0, 94, 0, 1, 0, 11, 0, 0, 0, 224, 0, 6, 115, 97, 114, 97, 109, 97, 255, 255, 255, 255, 0, 0, 1, 244, 0, 0, 0, 1, 6, 64, 0, 0, 0, 0, 0, 0, 0, 255, 255, 255, 255, 0, 0, 0, 1, 0},
+			expected: &KafkaInfo{
+				ClientID:    "sarama",
+				Operation:   Fetch,
+				TopicOffset: 45,
+			},
+		},
+		{
 			name:  "Fetch request (v11) truncated",
 			input: []byte{0, 0, 0, 94, 0, 1, 0, 11, 0, 0, 0, 224, 0, 6, 115, 97, 114, 97, 109, 97, 255, 255, 255, 255, 0, 0, 1, 244, 0, 0, 0, 1, 6, 64, 0, 0, 0, 0, 0, 0, 0, 255, 255, 255, 255, 0, 0, 0, 1},
 			expected: &KafkaInfo{

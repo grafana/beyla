c89d80bc2ac1c84ae0e542c912b337a400361bf0
Mario Macias <mario.macias@grafana.com>
2024-10-14 17:19:44 +0200

    amend resplicaset testing

--- test/integration/k8s/common/k8s_metrics_testfuncs.go
+++ test/integration/k8s/common/k8s_metrics_testfuncs.go
@@ -123,7 +123,6 @@ func FeatureHTTPMetricsDecoration(manifest string) features.Feature {
 				"k8s_pod_start_time":  TimeRegex,
 				"k8s_owner_name":      "^testserver$",
 				"k8s_deployment_name": "^testserver$",
-				"k8s_replicaset_name": "^testserver-",
 				"k8s_cluster_name":    "^beyla$",
 			})).
 		Assess("all the span graph metrics exist",
@@ -166,7 +165,6 @@ func FeatureGRPCMetricsDecoration(manifest string) features.Feature {
 				"k8s_pod_start_time":  TimeRegex,
 				"k8s_owner_name":      "^testserver$",
 				"k8s_deployment_name": "^testserver$",
-				"k8s_replicaset_name": "^testserver-",
 				"k8s_cluster_name":    "^beyla$",
 			})).
 		Assess("target_info metrics exist",
@@ -185,7 +183,6 @@ func FeatureProcessMetricsDecoration(overrideProperties map[string]string) featu
 		"k8s_pod_uid":         UUIDRegex,
 		"k8s_pod_start_time":  TimeRegex,
 		"k8s_deployment_name": "^testserver$",
-		"k8s_replicaset_name": "^testserver-",
 		"k8s_cluster_name":    "^beyla$",
 	}
 	for k, v := range overrideProperties {
@@ -217,7 +214,6 @@ func FeatureDisableInformersAppMetricsDecoration() features.Feature {
 					"k8s_pod_uid":         UUIDRegex,
 					"k8s_pod_start_time":  TimeRegex,
 					"k8s_deployment_name": "^testserver$",
-					"k8s_replicaset_name": "^testserver-.*",
 					"k8s_cluster_name":    "^beyla$",
 				})).Feature()
 }

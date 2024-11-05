e11d36ce6eed6f3f80f87d7ccb98ec17337f47d8
Nikola Grcevski <grcevski@gmail.com>
2024-11-04 19:14:33 -0500

    Simplify TC L7 code - part 1

--- bpf/tc_http_tp.c
+++ bpf/tc_http_tp.c
@@ -14,30 +14,30 @@ enum { TC_ACT_OK = 0, TC_ACT_RECLASSIFY = 1, TC_ACT_SHOT = 2 };
 enum { MAX_IP_PACKET_SIZE = 0x7fff };
 enum { MAX_INLINE_LEN = 0x7ff };
 
-enum connection_state : __u8 { ESTABLISHED, FIN_WAIT_1, FIN_WAIT_2, CLOSING, CLOSE_WAIT, LAST_ACK };
+enum connection_state { ESTABLISHED, FIN_WAIT_1, FIN_WAIT_2, CLOSING, CLOSE_WAIT, LAST_ACK };
 
 struct tc_http_ctx {
-    __u32 xtra_bytes;
-    __u8 state;
+    u32 xtra_bytes;
+    u8 state;
 } __attribute__((packed));
 
 struct tc_http_ctx_map {
     __uint(type, BPF_MAP_TYPE_HASH);
-    __type(key, __u32);
+    __type(key, u32);
     __type(value, struct tc_http_ctx);
     __uint(max_entries, 10240);
 } tc_http_ctx_map SEC(".maps");
 
 const char TP[] = "Traceparent: 00-0123456789ABCDEFGHIJKLMNOPQRSTUV-0123456789ABCDEF-XX\r\n";
-const __u32 EXTEND_SIZE = sizeof(TP) - 1;
+const u32 EXTEND_SIZE = sizeof(TP) - 1;
 
 struct datasum_loop_ctx {
     const unsigned char *b;
     const unsigned char *e;
-    __u32 *sum;
+    u32 *sum;
 };
 
-static long calculate_datasum_loop(__u32 index, void *ctx) {
+static long calculate_datasum_loop(u32 index, void *ctx) {
     if (index % 2 == 1) {
         return 0;
     }
@@ -48,17 +48,17 @@ static long calculate_datasum_loop(__u32 index, void *ctx) {
 
     const unsigned char *b = lctx->b;
     const unsigned char *e = lctx->e;
-    __u32 *sum = lctx->sum;
+    u32 *sum = lctx->sum;
 
     if (!b) {
         return 1;
     }
 
-    if (&b[index] + sizeof(__u16) > e) {
+    if (&b[index] + sizeof(u16) > e) {
         return 1;
     }
 
-    __u16 word;
+    u16 word;
 
     __builtin_memcpy(&word, &b[index], sizeof(word));
 
@@ -68,13 +68,13 @@ static long calculate_datasum_loop(__u32 index, void *ctx) {
 }
 
 static __always_inline void
-calculate_datasum(const unsigned char *b, const unsigned char *e, __u32 *sum) {
+calculate_datasum(const unsigned char *b, const unsigned char *e, u32 *sum) {
     if (b >= e) {
         *sum = 0;
         return;
     }
 
-    const __u32 len = (e - b) & MAX_IP_PACKET_SIZE;
+    const u32 len = (e - b) & MAX_IP_PACKET_SIZE;
 
     struct datasum_loop_ctx datasum_loop_ctx = {b, e, sum};
 
@@ -86,7 +86,7 @@ calculate_datasum(const unsigned char *b, const unsigned char *e, __u32 *sum) {
             return;
         }
 
-        __u16 word = 0;
+        u16 word = 0;
         __builtin_memcpy(&word, &b[len - 1], 1);
 
         *sum += word;
@@ -98,20 +98,20 @@ calculate_datasum(const unsigned char *b, const unsigned char *e, __u32 *sum) {
 }
 
 static __always_inline void
-calculate_datasum_inline(const unsigned char *b, const unsigned char *e, __u32 *sum) {
+calculate_datasum_inline(const unsigned char *b, const unsigned char *e, u32 *sum) {
     if (b >= e) {
         *sum = 0;
         return;
     }
 
-    const __u32 len = (e - b) & MAX_INLINE_LEN;
+    const u32 len = (e - b) & MAX_INLINE_LEN;
 
-    for (__u32 i = 0; i < len; i += 2) {
+    for (u32 i = 0; i < len; i += 2) {
         if (&b[i] + 1 >= e) {
             break;
         }
 
-        __u16 word;
+        u16 word;
 
         __builtin_memcpy(&word, &b[i], 2);
 
@@ -124,7 +124,7 @@ calculate_datasum_inline(const unsigned char *b, const unsigned char *e, __u32 *
             return;
         }
 
-        __u16 word = 0;
+        u16 word = 0;
         __builtin_memcpy(&word, &b[len - 1], 1);
 
         *sum += word;
@@ -136,10 +136,10 @@ calculate_datasum_inline(const unsigned char *b, const unsigned char *e, __u32 *
 }
 
 static __always_inline void
-encode_hex_skb(unsigned char *dst, const unsigned char *src, __u32 src_len) {
+encode_hex_skb(unsigned char *dst, const unsigned char *src, u32 src_len) {
 
 #pragma clang loop unroll(full)
-    for (__u32 i = 0, j = 0; i < src_len; i++) {
+    for (u32 i = 0, j = 0; i < src_len; i++) {
         unsigned char p = src[i];
 
         dst[j++] = hex[(p >> 4) & 0xff];
@@ -151,7 +151,7 @@ encode_hex_skb(unsigned char *dst, const unsigned char *src, __u32 src_len) {
 // validated
 static __always_inline unsigned char *
 check_pkt_access(unsigned char *buf, //NOLINT(readability-non-const-parameter)
-                 __u32 offset,
+                 u32 offset,
                  const unsigned char *end) {
     unsigned char *ret;
 
@@ -234,7 +234,7 @@ static __always_inline void *ctx_data_end(struct __sk_buff *ctx) {
     return data_end;
 }
 
-[[maybe_unused]] static __always_inline struct ethhdr *eth_header(struct __sk_buff *ctx) {
+__attribute__((unused)) static __always_inline struct ethhdr *eth_header(struct __sk_buff *ctx) {
     void *data = ctx_data(ctx);
 
     return (data + sizeof(struct ethhdr) > ctx_data_end(ctx)) ? NULL : data;
@@ -292,11 +292,11 @@ static __always_inline int update_tcp_csum(struct __sk_buff *ctx) {
 
     tcp->check = 0;
 
-    const __u16 tcp_len = (e - b) & MAX_IP_PACKET_SIZE;
+    const u16 tcp_len = (e - b) & MAX_IP_PACKET_SIZE;
 
-    __u32 data_sum = 0;
+    u32 data_sum = 0;
 
-    __u32 pseudo_header[3];
+    u32 pseudo_header[3];
     pseudo_header[0] = iph->saddr;
     pseudo_header[1] = iph->daddr;
     pseudo_header[2] = bpf_htonl((6 << 16) | tcp_len);
@@ -310,7 +310,7 @@ static __always_inline int update_tcp_csum(struct __sk_buff *ctx) {
         calculate_datasum((unsigned char *)tcp, e, &data_sum);
     }
 
-    const __u16 tcp_csum = (__u16)~data_sum;
+    const u16 tcp_csum = (u16)~data_sum;
 
     tcp = tcp_header(ctx);
 
@@ -330,11 +330,11 @@ static __always_inline long update_ip_csum(struct __sk_buff *ctx) {
         return -1;
     }
 
-    const __u16 new_total_len = bpf_ntohs(iph->tot_len) + EXTEND_SIZE;
+    const u16 new_total_len = bpf_ntohs(iph->tot_len) + EXTEND_SIZE;
     const __be16 ip_tot_len_old = iph->tot_len;
     const __be16 ip_tot_len_new = bpf_htons(new_total_len);
 
-    const __u32 ip_csum_off = (sizeof(struct ethhdr) + offsetof(struct iphdr, check)) & 0xff;
+    const u32 ip_csum_off = (sizeof(struct ethhdr) + offsetof(struct iphdr, check)) & 0xff;
 
     return bpf_l3_csum_replace(
         ctx, ip_csum_off, ip_tot_len_old, ip_tot_len_new, sizeof(ip_tot_len_new));
@@ -347,34 +347,20 @@ static __always_inline int is_http_request(struct __sk_buff *ctx) {
         return 0;
     }
 
-    char req_buf[] = "OPTIONS /"; // largest HTTP request operation
+    unsigned char req_buf[] = "OPTIONS /"; // largest HTTP request operation
 
-    const __u32 offset = (void *)payload - ctx_data(ctx);
+    const u32 offset = (void *)payload - ctx_data(ctx);
 
     if (bpf_skb_load_bytes(ctx, offset, &req_buf, sizeof(req_buf) - 1) != 0) {
         return 0;
     }
 
-    return (req_buf[0] == 'G' && req_buf[1] == 'E' && req_buf[2] == 'T' && req_buf[3] == ' ' &&
-            req_buf[4] == '/') ||
-           (req_buf[0] == 'P' && req_buf[1] == 'O' && req_buf[2] == 'S' && req_buf[3] == 'T' &&
-            req_buf[4] == ' ' && req_buf[5] == '/') ||
-           (req_buf[0] == 'P' && req_buf[1] == 'U' && req_buf[2] == 'T' && req_buf[3] == ' ' &&
-            req_buf[4] == '/') ||
-           (req_buf[0] == 'P' && req_buf[1] == 'A' && req_buf[2] == 'T' && req_buf[3] == 'C' &&
-            req_buf[4] == 'H' && req_buf[5] == ' ' && req_buf[5] == '/') ||
-           (req_buf[0] == 'D' && req_buf[1] == 'E' && req_buf[2] == 'L' && req_buf[3] == 'E' &&
-            req_buf[4] == 'T' && req_buf[5] == 'E' && req_buf[6] == ' ' && req_buf[7] == '/') ||
-           (req_buf[0] == 'H' && req_buf[1] == 'E' && req_buf[2] == 'A' && req_buf[3] == 'D' &&
-            req_buf[4] == ' ' && req_buf[5] == '/') ||
-           (req_buf[0] == 'O' && req_buf[1] == 'P' && req_buf[1] == 'T' && req_buf[1] == 'I' &&
-            req_buf[1] == 'O' && req_buf[1] == 'N' && req_buf[1] == 'S' && req_buf[1] == ' ' &&
-            req_buf[1] == '/');
+    return is_http_request_buf(req_buf, 9);
 }
 
 static __always_inline unsigned char *
-memchar(unsigned char *haystack, char needle, const unsigned char *end, __u32 size) {
-    for (__u32 i = 0; i < size; ++i) {
+memchar(unsigned char *haystack, char needle, const unsigned char *end, u32 size) {
+    for (u32 i = 0; i < size; ++i) {
         if (&haystack[i] >= end) {
             break;
         }
@@ -393,17 +379,17 @@ struct memmove_loop_ctx {
 
     const unsigned char *end;
 
-    __u32 size;
+    u32 size;
 };
 
-static long memmove_loop(__u32 index, void *ctx) {
+static long memmove_loop(u32 index, void *ctx) {
     struct memmove_loop_ctx *lctx = (struct memmove_loop_ctx *)ctx;
 
     if (index == lctx->size) {
         return 1;
     }
 
-    const __u32 i = (lctx->size - index) & MAX_IP_PACKET_SIZE;
+    const u32 i = (lctx->size - index) & MAX_IP_PACKET_SIZE;
 
     const unsigned char *end = lctx->end;
 
@@ -425,7 +411,7 @@ static long memmove_loop(__u32 index, void *ctx) {
 }
 
 static __always_inline void
-move_data(unsigned char *dst, unsigned char *src, const unsigned char *end, __u32 size) {
+move_data(unsigned char *dst, unsigned char *src, const unsigned char *end, u32 size) {
     struct memmove_loop_ctx memmove_loop_ctx = {dst, src, end, size};
 
     bpf_loop(size, memmove_loop, &memmove_loop_ctx, 0);
@@ -453,8 +439,8 @@ static __always_inline int extend_skb(struct __sk_buff *ctx, const tp_info_t *tp
         return 0;
     }
 
-    const __u32 copy_size = ((unsigned char *)ctx_data_end(ctx) - newline - 1) & MAX_IP_PACKET_SIZE;
-    const __u32 nl_offset = newline - payload;
+    const u32 copy_size = ((unsigned char *)ctx_data_end(ctx) - newline - 1) & MAX_IP_PACKET_SIZE;
+    const u32 nl_offset = newline - payload;
 
     if (bpf_skb_change_tail(ctx, ctx->len + EXTEND_SIZE, 0) != 0) {
         return 0;
@@ -500,7 +486,7 @@ static __always_inline int extend_skb(struct __sk_buff *ctx, const tp_info_t *tp
     return update_tcp_csum(ctx);
 }
 
-static __always_inline void update_tcp_seq(struct __sk_buff *ctx, __u32 extra_bytes) {
+static __always_inline void update_tcp_seq(struct __sk_buff *ctx, u32 extra_bytes) {
     if (extra_bytes == 0) {
         return;
     }
@@ -511,14 +497,14 @@ static __always_inline void update_tcp_seq(struct __sk_buff *ctx, __u32 extra_by
         return;
     }
 
-    __u32 seq = bpf_ntohl(tcp->seq);
+    u32 seq = bpf_ntohl(tcp->seq);
     seq += extra_bytes;
 
     tcp->seq = bpf_htonl(seq);
 }
 
 static __always_inline void
-get_extra_xmited_bytes(__u32 key, __u32 *extra_bytes, struct tc_http_ctx **http_ctx) {
+get_extra_xmited_bytes(u32 key, u32 *extra_bytes, struct tc_http_ctx **http_ctx) {
     struct tc_http_ctx *ctx = bpf_map_lookup_elem(&tc_http_ctx_map, &key);
 
     if (ctx) {
@@ -530,12 +516,12 @@ get_extra_xmited_bytes(__u32 key, __u32 *extra_bytes, struct tc_http_ctx **http_
     }
 }
 
-static __always_inline void delete_http_ctx(__u32 key) {
+static __always_inline void delete_http_ctx(u32 key) {
     bpf_map_delete_elem(&tc_http_ctx_map, &key);
 }
 
 static __always_inline void
-update_conn_state_egress(struct tcphdr *tcp, struct tc_http_ctx *http_ctx, __u32 key) {
+update_conn_state_egress(struct tcphdr *tcp, struct tc_http_ctx *http_ctx, u32 key) {
     if (tcp->fin) {
         if (http_ctx->state == ESTABLISHED) {
             // we've initiated a connection shutdown
@@ -550,7 +536,7 @@ update_conn_state_egress(struct tcphdr *tcp, struct tc_http_ctx *http_ctx, __u32
 }
 
 static __always_inline void
-update_conn_state_ingress(struct tcphdr *tcp, struct tc_http_ctx *http_ctx, __u32 key) {
+update_conn_state_ingress(struct tcphdr *tcp, struct tc_http_ctx *http_ctx, u32 key) {
     if (tcp->rst) {
         delete_http_ctx(key);
         return;
@@ -568,14 +554,10 @@ update_conn_state_ingress(struct tcphdr *tcp, struct tc_http_ctx *http_ctx, __u3
             // we've only received ACK, but no FIN, wait for it
             http_ctx->state = FIN_WAIT_2;
         }
-    } else if (http_ctx->state == CLOSING && tcp->ack) {
-        // we've entered TIME_WAIT - connection is closed
-        delete_http_ctx(key);
-    } else if (http_ctx->state == LAST_ACK && tcp->ack) {
-        // we've entered TIME_WAIT - connection is closed
-        delete_http_ctx(key);
-    } else if (http_ctx->state == FIN_WAIT_2 && tcp->fin) {
-        // we've entered TIME_WAIT - connection is closed
+    } else if ((http_ctx->state == CLOSING && tcp->ack) ||
+               (http_ctx->state == LAST_ACK && tcp->ack) ||
+               (http_ctx->state == FIN_WAIT_2 && tcp->fin)) {
+        // we've entered a connection is closed condition
         delete_http_ctx(key);
     } else if (tcp->fin && http_ctx->state == ESTABLISHED) {
         // the peeer has initiated closing the connection
@@ -591,11 +573,11 @@ int tc_http_egress(struct __sk_buff *ctx) {
         return TC_ACT_OK;
     }
 
-    const __u16 src_port = bpf_ntohs(tcp->source);
-    const __u16 dst_port = bpf_ntohs(tcp->dest);
-    const __u32 key = src_port;
+    const u16 src_port = bpf_ntohs(tcp->source);
+    const u16 dst_port = bpf_ntohs(tcp->dest);
+    const u32 key = src_port;
 
-    __u32 extra_bytes = 0;
+    u32 extra_bytes = 0;
     struct tc_http_ctx *http_ctx = NULL;
 
     get_extra_xmited_bytes(src_port, &extra_bytes, &http_ctx);
@@ -660,8 +642,8 @@ int tc_http_ingress(struct __sk_buff *ctx) {
         return TC_ACT_OK;
     }
 
-    const __u16 dst_port = bpf_ntohs(tcp->dest);
-    const __u32 key = dst_port;
+    const u16 dst_port = bpf_ntohs(tcp->dest);
+    const u32 key = dst_port;
 
     struct tc_http_ctx *http_ctx = bpf_map_lookup_elem(&tc_http_ctx_map, &key);
 
@@ -669,7 +651,7 @@ int tc_http_ingress(struct __sk_buff *ctx) {
         return TC_ACT_OK;
     }
 
-    __u32 ack_seq = bpf_ntohl(tcp->ack_seq);
+    u32 ack_seq = bpf_ntohl(tcp->ack_seq);
     ack_seq -= http_ctx->xtra_bytes;
 
     tcp->ack_seq = bpf_htonl(ack_seq);

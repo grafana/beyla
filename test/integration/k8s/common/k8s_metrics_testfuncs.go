71b1d6c3825bf21996de0c2f540f061b43d1e7eb
Mario Macias <mario.macias@grafana.com>
2025-01-20 17:27:57 +0100

    TEsting prom metrics

--- test/integration/k8s/common/k8s_metrics_testfuncs.go
+++ test/integration/k8s/common/k8s_metrics_testfuncs.go
@@ -118,6 +118,12 @@ func FeatureHTTPMetricsDecoration(manifest string, overrideAttrs map[string]stri
 		"host_name":                "testserver",
 		"host_id":                  HostIDRegex,
 	}
+	// if service_instance_id is overridden to be empty, we will check that value for target_info{instance} instead
+	if overrideAttrs != nil {
+		if sid, ok := overrideAttrs["service_instance_id"]; ok && sid == "" {
+			allAttributes["instance"] = sid
+		}
+	}
 
 	return features.New("Decoration of Pod-to-Service communications").
 		Setup(pinger.Deploy()).
@@ -153,6 +159,7 @@ func FeatureHTTPMetricsDecoration(manifest string, overrideAttrs map[string]stri
 			attributeMap(allAttributes, overrideAttrs,
 				"host_name",
 				"host_id",
+				"instance",
 			)),
 	).Feature()
 }
@@ -175,7 +182,7 @@ func attributeMap(original, override map[string]string, fields ...string) map[st
 	return result
 }
 
-func FeatureGRPCMetricsDecoration(manifest string) features.Feature {
+func FeatureGRPCMetricsDecoration(manifest string, overrideAttrs map[string]string) features.Feature {
 	pinger := kube.Template[Pinger]{
 		TemplateFile: manifest,
 		Data: Pinger{
@@ -183,33 +190,39 @@ func FeatureGRPCMetricsDecoration(manifest string) features.Feature {
 			TargetURL: "testserver:5051",
 		},
 	}
+
+	allAttributes := map[string]string{
+		"k8s_namespace_name":  "^default$",
+		"k8s_node_name":       ".+-control-plane$",
+		"k8s_pod_uid":         UUIDRegex,
+		"k8s_pod_start_time":  TimeRegex,
+		"k8s_cluster_name":    "^beyla$",
+		"k8s_owner_name":      "^testserver$",
+		"k8s_deployment_name": "^testserver$",
+		"k8s_replicaset_name": "^testserver-",
+		"service_instance_id": "^default\\.testserver-.+\\.testserver",
+	}
+	// if service_instance_id is overridden to be empty, we will check that value for target_info{instance} instead
+	targetInfoInstance := ""
+	if overrideAttrs != nil {
+		if sid, ok := overrideAttrs["service_instance_id"]; ok && sid == "" {
+			targetInfoInstance = allAttributes["service_instance_id"]
+		}
+	}
 	return features.New("Decoration of Pod-to-Service communications").
 		Setup(pinger.Deploy()).
 		Teardown(pinger.Delete()).
 		Assess("all the client metrics are properly decorated",
-			testMetricsDecoration(grpcClientMetrics, `{k8s_pod_name="internal-grpc-pinger"}`, map[string]string{
-				"k8s_namespace_name": "^default$",
-				"k8s_node_name":      ".+-control-plane$",
-				"k8s_pod_uid":        UUIDRegex,
-				"k8s_pod_start_time": TimeRegex,
-				"k8s_cluster_name":   "^beyla$",
-			}, "k8s_deployment_name")).
+			testMetricsDecoration(grpcClientMetrics, `{k8s_pod_name="internal-grpc-pinger"}`,
+				attributeMap(allAttributes, overrideAttrs), "k8s_deployment_name")).
 		Assess("all the server metrics are properly decorated",
-			testMetricsDecoration(grpcServerMetrics, `{k8s_pod_name=~"testserver-.*"}`, map[string]string{
-				"k8s_namespace_name":  "^default$",
-				"k8s_node_name":       ".+-control-plane$",
-				"k8s_pod_uid":         UUIDRegex,
-				"k8s_pod_start_time":  TimeRegex,
-				"k8s_owner_name":      "^testserver$",
-				"k8s_deployment_name": "^testserver$",
-				"k8s_replicaset_name": "^testserver-",
-				"k8s_cluster_name":    "^beyla$",
-				"service_instance_id": "^default\\.testserver-.+\\.testserver",
-			})).
+			testMetricsDecoration(grpcServerMetrics, `{k8s_pod_name=~"testserver-.*"}`,
+				attributeMap(allAttributes, overrideAttrs))).
 		Assess("target_info metrics exist",
 			testMetricsDecoration([]string{"target_info"}, `{job=~".*testserver"}`, map[string]string{
 				"host_name": "testserver",
 				"host_id":   HostIDRegex,
+				"instance":  targetInfoInstance,
 			}),
 		).Feature()
 }
--- test/integration/k8s/prom/k8s_prom_test.go
+++ test/integration/k8s/prom/k8s_prom_test.go
@@ -15,11 +15,17 @@ func TestPrometheus_MetricsDecoration_AA_WaitForComponents(t *testing.T) {
 }
 
 func TestPrometheus_MetricsDecoration_HTTP(t *testing.T) {
-	cluster.TestEnv().Test(t, k8s.FeatureHTTPMetricsDecoration(k8s.PingerManifestProm, nil))
+	cluster.TestEnv().Test(t, k8s.FeatureHTTPMetricsDecoration(k8s.PingerManifestProm, map[string]string{
+		// service_instance_id is reported in target_info for prometheus metrics. Will check in another test
+		"service_instance_id": "",
+	}))
 }
 
 func TestPrometheus_MetricsDecoration_GRPC(t *testing.T) {
-	cluster.TestEnv().Test(t, k8s.FeatureGRPCMetricsDecoration(k8s.GrpcPingerManifestProm))
+	cluster.TestEnv().Test(t, k8s.FeatureGRPCMetricsDecoration(k8s.GrpcPingerManifestProm, map[string]string{
+		// service_instance_id is reported in target_info for prometheus metrics. Will check in another test
+		"service_instance_id": "",
+	}))
 }
 
 func TestPrometheus_ProcessMetrics(t *testing.T) {

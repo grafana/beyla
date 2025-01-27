d2ec6f1b314bbe31fb786eeec7f8b12d31d53db6
Mario Macias <mario.macias@grafana.com>
2025-01-27 11:55:53 +0100

    Simplify process FindAndInstrument synchronization

--- pkg/components/beyla.go
+++ pkg/components/beyla.go
@@ -70,11 +70,9 @@ func RunBeyla(ctx context.Context, cfg *beyla.Config) error {
 func setupAppO11y(ctx context.Context, ctxInfo *global.ContextInfo, config *beyla.Config) error {
 	slog.Info("starting Beyla in Application Observability mode")
 
-	wg := sync.WaitGroup{}
-	defer wg.Wait()
-
 	instr := appolly.New(ctx, ctxInfo, config)
-	if err := instr.FindAndInstrument(&wg); err != nil {
+	finderDone, err := instr.FindAndInstrument()
+	if err != nil {
 		slog.Debug("can't find  target process", "error", err)
 		return fmt.Errorf("can't find target process: %w", err)
 	}
@@ -82,6 +80,7 @@ func setupAppO11y(ctx context.Context, ctxInfo *global.ContextInfo, config *beyl
 		slog.Debug("can't start read and forwarding", "error", err)
 		return fmt.Errorf("can't start read and forwarding: %w", err)
 	}
+	<-finderDone
 	return nil
 }
 

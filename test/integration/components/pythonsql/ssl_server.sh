33bf0cc42b9156023e7cc9befdd23cefcc58ae82
Nikola Grcevski <grcevski@gmail.com>
2025-02-08 15:48:56 -0500

    add timeout to server run command

--- test/integration/components/pythonsql/ssl_server.sh
+++ test/integration/components/pythonsql/ssl_server.sh
@@ -1,1 +1,1 @@
-gunicorn --keyfile server.key --certfile server.crt -w 4 -b 0.0.0.0:8380 -k uvicorn.workers.UvicornWorker main_ssl:app
+gunicorn --timeout 120 --keyfile server.key --certfile server.crt -w 1 -b 0.0.0.0:8380 -k uvicorn.workers.UvicornWorker main_ssl:app

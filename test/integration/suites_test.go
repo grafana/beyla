eaabbf891bc03cf38d13fc2de5efa32023eab6af
Nikola Grcevski <grcevski@gmail.com>
2025-02-13 15:27:18 -0500

    add integration test

--- test/integration/suites_test.go
+++ test/integration/suites_test.go
@@ -32,6 +32,7 @@ func TestSuite(t *testing.T) {
 	t.Run("GRPC RED metrics", testREDMetricsGRPC)
 	t.Run("GRPC TLS RED metrics", testREDMetricsGRPCTLS)
 	t.Run("Internal Prometheus metrics", testInternalPrometheusExport)
+	t.Run("Exemplars exist", testExemplarsExist)
 
 	require.NoError(t, compose.Close())
 }

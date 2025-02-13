2ad99ec5fb763b293c54cb367c9ea90278ef2953
Nikola Grcevski <grcevski@gmail.com>
2025-02-13 13:52:10 -0500

    adjust test offsets

--- pkg/internal/ebpf/common/pids_test.go
+++ pkg/internal/ebpf/common/pids_test.go
@@ -6,6 +6,7 @@ import (
 	"time"
 
 	"github.com/stretchr/testify/assert"
+	"go.opentelemetry.io/otel/trace"
 
 	"github.com/grafana/beyla/pkg/internal/request"
 	"github.com/grafana/beyla/pkg/internal/svc"
@@ -36,7 +37,7 @@ func TestFilter_SameNS(t *testing.T) {
 		{Pid: request.PidInfo{UserPID: 123, HostPID: 333, Namespace: 33}},
 		{Pid: request.PidInfo{UserPID: 456, HostPID: 666, Namespace: 33}},
 		{Pid: request.PidInfo{UserPID: 789, HostPID: 234, Namespace: 33}},
-	}, pf.Filter(spanSet))
+	}, resetTraceContext(pf.Filter(spanSet)))
 }
 
 func TestFilter_DifferentNS(t *testing.T) {
@@ -50,7 +51,7 @@ func TestFilter_DifferentNS(t *testing.T) {
 
 	// with the same namespace, it filters by user PID, as it is the PID
 	// that is seen by Beyla's process discovery
-	assert.Equal(t, []request.Span{}, pf.Filter(spanSet))
+	assert.Equal(t, []request.Span{}, resetTraceContext(pf.Filter(spanSet)))
 }
 
 func TestFilter_Block(t *testing.T) {
@@ -67,7 +68,7 @@ func TestFilter_Block(t *testing.T) {
 	assert.EventuallyWithT(t, func(c *assert.CollectT) {
 		assert.Equal(c, []request.Span{
 			{Pid: request.PidInfo{UserPID: 456, HostPID: 666, Namespace: 33}},
-		}, pf.Filter(spanSet))
+		}, resetTraceContext(pf.Filter(spanSet)))
 	}, 10*time.Second, 10*time.Millisecond, "still haven't seen pid 123 as blocked")
 }
 
@@ -86,7 +87,7 @@ func TestFilter_NewNSLater(t *testing.T) {
 		{Pid: request.PidInfo{UserPID: 123, HostPID: 333, Namespace: 33}},
 		{Pid: request.PidInfo{UserPID: 456, HostPID: 666, Namespace: 33}},
 		{Pid: request.PidInfo{UserPID: 789, HostPID: 234, Namespace: 33}},
-	}, pf.Filter(spanSet))
+	}, resetTraceContext(pf.Filter(spanSet)))
 
 	pf.AllowPID(1000, 44, &svc.Attrs{}, PIDTypeGo)
 
@@ -95,7 +96,7 @@ func TestFilter_NewNSLater(t *testing.T) {
 		{Pid: request.PidInfo{UserPID: 456, HostPID: 666, Namespace: 33}},
 		{Pid: request.PidInfo{UserPID: 789, HostPID: 234, Namespace: 33}},
 		{Pid: request.PidInfo{UserPID: 1000, HostPID: 1234, Namespace: 44}},
-	}, pf.Filter(spanSet))
+	}, resetTraceContext(pf.Filter(spanSet)))
 
 	pf.BlockPID(456, 33)
 
@@ -103,14 +104,14 @@ func TestFilter_NewNSLater(t *testing.T) {
 		{Pid: request.PidInfo{UserPID: 123, HostPID: 333, Namespace: 33}},
 		{Pid: request.PidInfo{UserPID: 789, HostPID: 234, Namespace: 33}},
 		{Pid: request.PidInfo{UserPID: 1000, HostPID: 1234, Namespace: 44}},
-	}, pf.Filter(spanSet))
+	}, resetTraceContext(pf.Filter(spanSet)))
 
 	pf.BlockPID(1000, 44)
 
 	assert.Equal(t, []request.Span{
 		{Pid: request.PidInfo{UserPID: 123, HostPID: 333, Namespace: 33}},
 		{Pid: request.PidInfo{UserPID: 789, HostPID: 234, Namespace: 33}},
-	}, pf.Filter(spanSet))
+	}, resetTraceContext(pf.Filter(spanSet)))
 }
 
 func TestFilter_ExportsOTelDetection(t *testing.T) {
@@ -134,4 +135,14 @@ func TestFilter_ExportsOTelDetection(t *testing.T) {
 	checkIfExportsOTel(&s, &span)
 	assert.False(t, s.ExportsOTelMetrics())
 	assert.True(t, s.ExportsOTelTraces())
+}
+
+func resetTraceContext(spans []request.Span) []request.Span {
+	for i := range spans {
+		spans[i].TraceID = trace.TraceID{0}
+		spans[i].SpanID = trace.SpanID{0}
+		spans[i].Flags = 0
+	}
+
+	return spans
 }

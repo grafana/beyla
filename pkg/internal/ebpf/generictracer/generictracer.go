8307d55113750b2bb323693f91fe4384f4aeb3f4
Nikola Grcevski <grcevski@gmail.com>
2025-02-21 10:20:25 -0500

    Fix marshaling bug

--- pkg/internal/ebpf/generictracer/generictracer.go
+++ pkg/internal/ebpf/generictracer/generictracer.go
@@ -516,7 +516,7 @@ func (p *Tracer) watchForMisclassifedEvents() {
 			if p.bpfObjects.OngoingHttp2Connections != nil {
 				err := p.bpfObjects.OngoingHttp2Connections.Put(
 					&bpfPidConnectionInfoT{Conn: bpfConnectionInfoT(e.TCPInfo.ConnInfo), Pid: e.TCPInfo.Pid.HostPid},
-					uint8(e.TCPInfo.Ssl), // no new connection flag (0x3)
+					bpfHttp2ConnInfoDataT{Flags: e.TCPInfo.Ssl, Id: 0}, // no new connection flag (0x3)
 				)
 				if err != nil {
 					p.log.Debug("error writing HTTP2/gRPC connection info", "error", err)

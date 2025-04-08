cc73cf3c125a651bb9b4296eb4c17ebc16686830
Rafael Roquetto <rafael.roquetto@grafana.com>
2025-04-08 08:07:15 -0600

    Fix formatting/linter

--- bpf/imports.go
+++ bpf/imports.go
@@ -2,16 +2,18 @@
 
 package bpf
 
-import _ "github.com/grafana/beyla/v2/bpf/bpfcore"
-import _ "github.com/grafana/beyla/v2/bpf/common"
-import _ "github.com/grafana/beyla/v2/bpf/generictracer"
-import _ "github.com/grafana/beyla/v2/bpf/gotracer"
-import _ "github.com/grafana/beyla/v2/bpf/gpuevent"
-import _ "github.com/grafana/beyla/v2/bpf/httptracer"
-import _ "github.com/grafana/beyla/v2/bpf/logger"
-import _ "github.com/grafana/beyla/v2/bpf/maps"
-import _ "github.com/grafana/beyla/v2/bpf/netolly"
-import _ "github.com/grafana/beyla/v2/bpf/pid"
-import _ "github.com/grafana/beyla/v2/bpf/rdns"
-import _ "github.com/grafana/beyla/v2/bpf/tctracer"
-import _ "github.com/grafana/beyla/v2/bpf/watcher"
+import (
+	_ "github.com/grafana/beyla/v2/bpf/bpfcore"
+	_ "github.com/grafana/beyla/v2/bpf/common"
+	_ "github.com/grafana/beyla/v2/bpf/generictracer"
+	_ "github.com/grafana/beyla/v2/bpf/gotracer"
+	_ "github.com/grafana/beyla/v2/bpf/gpuevent"
+	_ "github.com/grafana/beyla/v2/bpf/httptracer"
+	_ "github.com/grafana/beyla/v2/bpf/logger"
+	_ "github.com/grafana/beyla/v2/bpf/maps"
+	_ "github.com/grafana/beyla/v2/bpf/netolly"
+	_ "github.com/grafana/beyla/v2/bpf/pid"
+	_ "github.com/grafana/beyla/v2/bpf/rdns"
+	_ "github.com/grafana/beyla/v2/bpf/tctracer"
+	_ "github.com/grafana/beyla/v2/bpf/watcher"
+)

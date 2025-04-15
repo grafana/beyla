2a09e8c9fab122ab225b0da103d45713b49a0959
Mario Macias <mario.macias@grafana.com>
2025-04-15 16:24:04 +0200

    Fixed leaked failure

--- pkg/internal/ebpf/common/ringbuf_test.go
+++ pkg/internal/ebpf/common/ringbuf_test.go
@@ -78,7 +78,7 @@ func TestForwardRingbuf_CapacityFull(t *testing.T) {
 
 func TestForwardRingbuf_Deadline(t *testing.T) {
 	// GIVEN a ring buffer forwarder
-	ringBuf := &fakeRingBufReader{events: make(chan HTTPRequestTrace, 100), closeCh: make(chan struct{})}
+	ringBuf := replaceTestRingBuf(
 
 	metrics := &metricsReporter{}
 	forwardedMessagesQueue := msg.NewQueue[[]request.Span](msg.ChannelBufferLen(100))

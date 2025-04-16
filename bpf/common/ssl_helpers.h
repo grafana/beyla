6ff6e1fa1571d0caaea1a861eacd4eefe0ae1baf
Nikola Grcevski <grcevski@gmail.com>
2025-04-16 19:19:57 -0400

    restrict SSL to connection mapping by direction

--- bpf/common/ssl_helpers.h
+++ bpf/common/ssl_helpers.h
@@ -4,6 +4,7 @@
 #include <bpfcore/bpf_helpers.h>
 
 #include <common/connection_info.h>
+#include <common/protocol_defs.h>
 
 #include <logger/bpf_dbg.h>
 
@@ -21,13 +22,28 @@ static __always_inline void set_active_ssl_connection(pid_connection_info_t *con
     bpf_map_update_elem(&ssl_to_conn, &ssl, conn, BPF_ANY);
 }
 
-static __always_inline void *is_ssl_connection(u64 id, pid_connection_info_t *conn) {
+static __always_inline void *is_ssl_connection(u64 id, pid_connection_info_t *conn, u8 direction) {
     void *ssl = 0;
-    // Checks if it's sandwitched between active SSL handshake, read or write uprobe/uretprobe
-    ssl_args_t *ssl_args = bpf_map_lookup_elem(&active_ssl_read_args, &id);
+    ssl_args_t *ssl_args = 0;
+    u8 update_info = 0;
 
-    if (!ssl_args) {
+    // Checks if it's sandwitched between read or write uprobe/uretprobe
+    if (direction == TCP_RECV) {
+        ssl_args = bpf_map_lookup_elem(&active_ssl_read_args, &id);
+        if (ssl_args) {
+            update_info = 1;
+        } else {
+            ssl_args = bpf_map_lookup_elem(&active_ssl_write_args, &id);
+        }
+    } else if (direction == TCP_SEND) {
         ssl_args = bpf_map_lookup_elem(&active_ssl_write_args, &id);
+        if (ssl_args) {
+            update_info = 1;
+        } else {
+            ssl_args = bpf_map_lookup_elem(&active_ssl_read_args, &id);
+        }
+    } else {
+        bpf_dbg_printk("unknown ssl connection direction, this is a bug");
     }
 
     if (ssl_args) {
@@ -38,7 +54,12 @@ static __always_inline void *is_ssl_connection(u64 id, pid_connection_info_t *co
         return bpf_map_lookup_elem(&active_ssl_connections, conn);
     }
 
-    set_active_ssl_connection(conn, ssl);
+    // We want to update the SSL to connection info, only if the
+    // direction of the SSL traffic matches the SSL operation.
+    // That is TCP_RECV = SSL_read, TCP_SEND = SSL_write.
+    if (update_info) {
+        set_active_ssl_connection(conn, ssl);
+    }
 
     return ssl;
 }

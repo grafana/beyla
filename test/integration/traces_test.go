ec79aa46421dc4e3dfe1815196b77f1ea283d5b7
Marc Tuduri <marctc@protonmail.com>
2025-04-16 11:37:29 +0200

    restore thing

--- test/integration/traces_test.go
+++ test/integration/traces_test.go
@@ -806,14 +806,16 @@ func testNestedHTTPTracesKProbes(t *testing.T, extended bool) {
 		assert.Empty(t, sd, sd.String())
 	}
 
-	// test now with a different version of Java thread pool
-	for i := 0; i < 10; i++ {
-		doHTTPGet(t, "http://localhost:8086/jtraceB", 200)
-	}
+	if extended {
+		// test now with a different version of Java thread pool
+		for i := 0; i < 10; i++ {
+			doHTTPGet(t, "http://localhost:8086/jtraceA", 200)
+		}
 
-	t.Run("Traces RestClient client /jtraceB", func(t *testing.T) {
-		ensureTracesMatch(t, "jtraceB")
-	})
+		t.Run("Traces RestClient client /jtraceA", func(t *testing.T) {
+			ensureTracesMatch(t, "jtraceA")
+		})
+	}
 }
 
 func ensureTracesMatch(t *testing.T, urlPath string) {

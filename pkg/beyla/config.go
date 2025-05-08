e8add0d7ad5e5704f28b7fadc96101d1f22a8148
Mario Macias <mario.macias@grafana.com>
2025-05-08 14:33:01 +0200

    Support OTEL env var substitution in YAML config

--- pkg/beyla/config.go
+++ pkg/beyla/config.go
@@ -380,6 +380,8 @@ func LoadConfig(file io.Reader) (*Config, error) {
 		if err != nil {
 			return nil, fmt.Errorf("reading YAML configuration: %w", err)
 		}
+		// replaces environment variables in YAML file
+		cfgBuf = config.ReplaceEnv(cfgBuf)
 		if err := yaml.Unmarshal(cfgBuf, &cfg); err != nil {
 			return nil, fmt.Errorf("parsing YAML configuration: %w", err)
 		}

65303c2387d151edaf4280c22d6a4e5fa8ad8e61
Martijn Evers <mevers@gk-software.com>
2025-05-14 09:45:56 +0200

    Remove child from log, make other logs more consistent

--- pkg/internal/discover/attacher.go
+++ pkg/internal/discover/attacher.go
@@ -118,7 +118,7 @@ func (ta *TraceAttacher) getTracer(ie *ebpf.Instrumentable) bool {
 		ta.log.Debug("new process for already instrumented executable",
 			"pid", ie.FileInfo.Pid,
 			"child", ie.ChildPids,
-			"exec", ie.FileInfo.CmdExePath)
+			"cmd", ie.FileInfo.CmdExePath)
 		ie.FileInfo.Service.SDKLanguage = ie.Type
 		// allowing the tracer to forward traces from the new PID and its children processes
 		ta.monitorPIDs(tracer, ie)
@@ -139,7 +139,6 @@ func (ta *TraceAttacher) getTracer(ie *ebpf.Instrumentable) bool {
 	ta.log.Info("instrumenting process",
 		"cmd", ie.FileInfo.CmdExePath,
 		"pid", ie.FileInfo.Pid,
-		"child", ie.ChildPids,
 		"ino", ie.FileInfo.Ino,
 		"type", ie.Type,
 		"service", ie.FileInfo.Service.UID.Name,
@@ -211,7 +210,7 @@ func (ta *TraceAttacher) getTracer(ie *ebpf.Instrumentable) bool {
 	ta.log.Debug("new executable for discovered process",
 		"pid", ie.FileInfo.Pid,
 		"child", ie.ChildPids,
-		"exec", ie.FileInfo.CmdExePath,
+		"cmd", ie.FileInfo.CmdExePath,
 		"type", ie.Type)
 	// allowing the tracer to forward traces from the discovered PID and its children processes
 	ta.monitorPIDs(tracer, ie)
@@ -266,7 +265,7 @@ func (ta *TraceAttacher) reuseTracer(tracer *ebpf.ProcessTracer, ie *ebpf.Instru
 	ta.log.Debug("reusing Generic tracer for",
 		"pid", ie.FileInfo.Pid,
 		"child", ie.ChildPids,
-		"exec", ie.FileInfo.CmdExePath,
+		"cmd", ie.FileInfo.CmdExePath,
 		"language", ie.Type)
 
 	ta.monitorPIDs(tracer, ie)
@@ -283,7 +282,7 @@ func (ta *TraceAttacher) updateTracerProbes(tracer *ebpf.ProcessTracer, ie *ebpf
 	ta.log.Debug("reusing Generic tracer for",
 		"pid", ie.FileInfo.Pid,
 		"child", ie.ChildPids,
-		"exec", ie.FileInfo.CmdExePath,
+		"cmd", ie.FileInfo.CmdExePath,
 		"language", ie.Type)
 
 	ta.monitorPIDs(tracer, ie)
@@ -338,7 +337,6 @@ func (ta *TraceAttacher) notifyProcessDeletion(ie *ebpf.Instrumentable) {
 		ta.log.Info("process ended for already instrumented executable",
 			"cmd", ie.FileInfo.CmdExePath,
 			"pid", ie.FileInfo.Pid,
-			"child", ie.ChildPids,
 			"ino", ie.FileInfo.Ino,
 			"type", ie.Type,
 			"service", ie.FileInfo.Service.UID.Name,

7a8e30fb513a0e63e2d863c1c7ddea92de073917
Nikola Grcevski <6207777+grcevski@users.noreply.github.com>
2025-05-22 17:52:20 -0400

    Fix http2 multiple connections issues (#1971)

--- test/integration/http2go_test.go
+++ test/integration/http2go_test.go
@@ -11,7 +11,6 @@ import (
 	"time"
 
 	"github.com/mariomac/guara/pkg/test"
-	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 
 	"github.com/grafana/beyla/v2/test/integration/components/docker"
@@ -67,12 +66,11 @@ func testREDMetricsForHTTP2Library(t *testing.T, route, svcNs string) {
 }
 
 func testNestedHTTP2Traces(t *testing.T, url string) {
-	t.Skip("seems flaky, we need to look into this")
 	var traceID string
 
 	var trace jaeger.Trace
 	test.Eventually(t, time.Duration(1)*time.Minute, func(t require.TestingT) {
-		resp, err := http.Get(jaegerQueryURL + "?service=client&operation=GET")
+		resp, err := http.Get(jaegerQueryURL + "?service=client&operation=GET%20%2F" + url)
 		require.NoError(t, err)
 		if resp == nil {
 			return
@@ -80,49 +78,33 @@ func testNestedHTTP2Traces(t *testing.T, url string) {
 		require.Equal(t, http.StatusOK, resp.StatusCode)
 		var tq jaeger.TracesQuery
 		require.NoError(t, json.NewDecoder(resp.Body).Decode(&tq))
-		traces := tq.FindBySpan(jaeger.Tag{Key: "url.full", Type: "string", Value: "/" + url})
+		traces := tq.FindBySpan(jaeger.Tag{Key: "http.request.method", Type: "string", Value: "GET"})
 		require.GreaterOrEqual(t, len(traces), 1)
 		trace = traces[0]
 	}, test.Interval(100*time.Millisecond))
 
-	// Check the information of the python parent span
-	res := trace.FindByOperationName("GET /"+url, "server")
+	// Check the information of the HTTP2 client span
+	res := trace.FindByOperationName("GET /"+url, "client")
 	require.Len(t, res, 1)
 	parent := res[0]
 	require.NotEmpty(t, parent.TraceID)
 	traceID = parent.TraceID
 	require.NotEmpty(t, parent.SpanID)
-	// check duration is at least 2us
-	assert.Less(t, (2 * time.Microsecond).Microseconds(), parent.Duration)
-	// check span attributes
-	sd := parent.Diff(
-		jaeger.Tag{Key: "http.request.method", Type: "string", Value: "GET"},
-		jaeger.Tag{Key: "http.response.status_code", Type: "int64", Value: float64(200)},
-		jaeger.Tag{Key: "url.path", Type: "string", Value: "/" + url},
-		jaeger.Tag{Key: "server.port", Type: "int64", Value: float64(7373)},
-		jaeger.Tag{Key: "http.route", Type: "string", Value: "/" + url},
-		jaeger.Tag{Key: "span.kind", Type: "string", Value: "server"},
-	)
-	assert.Empty(t, sd, sd.String())
 
-	// Check the information of the rails parent span
-	res = trace.FindByOperationName("GET /"+url, "client")
-	require.Len(t, res, 1)
-	parent = res[0]
-	require.NotEmpty(t, parent.TraceID)
-	require.Equal(t, traceID, parent.TraceID)
-	require.NotEmpty(t, parent.SpanID)
-	// check duration is at least 2us
-	assert.Less(t, (2 * time.Microsecond).Microseconds(), parent.Duration)
-	// check span attributes
-	sd = parent.Diff(
-		jaeger.Tag{Key: "http.request.method", Type: "string", Value: "GET"},
-		jaeger.Tag{Key: "http.response.status_code", Type: "int64", Value: float64(200)},
-		jaeger.Tag{Key: "url.full", Type: "string", Value: "/" + url},
-		jaeger.Tag{Key: "server.port", Type: "int64", Value: float64(7373)},
-		jaeger.Tag{Key: "span.kind", Type: "string", Value: "client"},
-	)
-	assert.Empty(t, sd, sd.String())
+	// Find the same traceID on a server span
+	test.Eventually(t, time.Duration(1)*time.Minute, func(t require.TestingT) {
+		resp, err := http.Get(jaegerQueryURL + "?service=server&operation=GET%20%2F" + url + "&traceID=" + traceID)
+		require.NoError(t, err)
+		if resp == nil {
+			return
+		}
+		require.Equal(t, http.StatusOK, resp.StatusCode)
+		var tq jaeger.TracesQuery
+		require.NoError(t, json.NewDecoder(resp.Body).Decode(&tq))
+		traces := tq.FindBySpan(jaeger.Tag{Key: "http.request.method", Type: "string", Value: "GET"})
+		require.GreaterOrEqual(t, len(traces), 1)
+		trace = traces[0]
+	}, test.Interval(100*time.Millisecond))
 }
 
 func TestHTTP2Go(t *testing.T) {
@@ -144,12 +126,9 @@ func TestHTTP2Go(t *testing.T) {
 		testREDMetricsForHTTP2Library(t, "/pingrt", "http2-go")
 	})
 
-	// Seems flaky, we need to look into this.
 	if !lockdown {
 		t.Run("Go RED metrics: http2 context propagation ", func(t *testing.T) {
-			testNestedHTTP2Traces(t, "ping")
 			testNestedHTTP2Traces(t, "pingdo")
-			testNestedHTTP2Traces(t, "pingrt")
 		})
 	}
 

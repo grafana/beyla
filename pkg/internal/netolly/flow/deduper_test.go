36dc582333f83feb6ebd1c1a19a4d6428ad4ed67
Mario Macias <mario.macias@grafana.com>
2025-06-13 13:01:25 +0200

    Fix tests and vendor

--- pkg/internal/netolly/flow/deduper_test.go
+++ pkg/internal/netolly/flow/deduper_test.go
@@ -23,12 +23,11 @@ import (
 	"testing"
 	"time"
 
+	"github.com/open-telemetry/opentelemetry-ebpf-instrumentation/pkg/components/netolly/ebpf"
 	"github.com/open-telemetry/opentelemetry-ebpf-instrumentation/pkg/components/testutil"
 	"github.com/open-telemetry/opentelemetry-ebpf-instrumentation/pkg/pipe/msg"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
-
-	"github.com/grafana/beyla/v2/pkg/internal/netolly/ebpf"
 )
 
 var oneIf1, oneIf2, twoIf1, twoIf2 *ebpf.Record
--- pkg/internal/netolly/flow/reverse_dns_test.go
+++ pkg/internal/netolly/flow/reverse_dns_test.go
@@ -5,12 +5,11 @@ import (
 	"testing"
 	"time"
 
+	"github.com/open-telemetry/opentelemetry-ebpf-instrumentation/pkg/components/netolly/ebpf"
 	"github.com/open-telemetry/opentelemetry-ebpf-instrumentation/pkg/components/testutil"
 	"github.com/open-telemetry/opentelemetry-ebpf-instrumentation/pkg/pipe/msg"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
-
-	"github.com/grafana/beyla/v2/pkg/internal/netolly/ebpf"
 )
 
 var srcIP = [16]uint8{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 255, 255, 140, 82, 121, 4}

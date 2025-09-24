791d09ef7def023e6dc15fc37d363eb64edeca1b
Rafael Roquetto <rafael.roquetto@grafana.com>
2025-09-24 08:25:34 -0600

    Update Beyla

--- pkg/beyla/config_test.go
+++ pkg/beyla/config_test.go
@@ -179,6 +179,7 @@ network:
 			CommonEndpoint:    "localhost:3131",
 			TracesEndpoint:    "localhost:3232",
 			MaxQueueSize:      4096,
+			BatchTimeout:      15 * time.Second,
 			ReportersCacheLen: ReporterLRUSize,
 			Instrumentations: []string{
 				instrumentations.InstrumentationALL,

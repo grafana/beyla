7ce585e5dfec7ca7d612fc6349483685acd428f2
Stephen Lang <skl@users.noreply.github.com>
2025-10-10 15:54:54 +0100

    Backport missing main commits from 2.6 release (#2232)
    * Update Beyla
    
    * fix test
    
    ---------
    
    Co-authored-by: Rafael Roquetto <rafael.roquetto@grafana.com>
    Co-authored-by: Nikola Grcevski <grcevski@gmail.com>

--- pkg/beyla/config.go
+++ pkg/beyla/config.go
@@ -103,6 +103,7 @@ var DefaultConfig = Config{
 		Protocol:          otelcfg.ProtocolUnset,
 		TracesProtocol:    otelcfg.ProtocolUnset,
 		MaxQueueSize:      4096,
+		BatchTimeout:      15 * time.Second,
 		ReportersCacheLen: ReporterLRUSize,
 		Instrumentations: []string{
 			instrumentations.InstrumentationALL,

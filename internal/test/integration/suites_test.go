8d7e3ff01088d24305c309a26f5630d034994dab
Mario Macias <mario.macias@grafana.com>
2025-12-05 17:43:28 +0100

    Fix extra python test

--- internal/test/integration/suites_test.go
+++ internal/test/integration/suites_test.go
@@ -395,9 +395,9 @@ func TestSuite_Python(t *testing.T) {
 	// hasn't received any request, their processes are still instrumented
 	t.Run("Checking process metrics", testProcesses(map[string]string{
 		"process_executable_name": "python",
-		"process_executable_path": "/usr/local/bin/python",
+		"process_executable_path": "/usr/local/bin/python3.14",
 		"process_command":         "gunicorn",
-		"process_command_line":    "/usr/local/bin/python /usr/local/bin/gunicorn -w 4 -b 0.0.0.0:8380 main:app --timeout 90",
+		"process_command_line":    "/usr/local/bin/python3.14 /usr/local/bin/gunicorn -w 4 -b 0.0.0.0:8380 main:app --timeout 90",
 	}))
 	t.Run("Python RED metrics", testREDMetricsPythonHTTP)
 	t.Run("Python RED metrics with timeouts", testREDMetricsTimeoutPythonHTTP)

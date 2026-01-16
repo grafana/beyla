1497159a0a25ec7a5bd55ab428221d141361177a
Stephen Lang <stephen.lang@grafana.com>
2026-01-16 15:29:36 +0000

    fix: testJavaKafkaLargeBuffer is unused

--- internal/test/integration/suites_test.go
+++ internal/test/integration/suites_test.go
@@ -424,6 +424,15 @@ func TestSuite_JavaKafka(t *testing.T) {
 	require.NoError(t, compose.Close())
 }
 
+func TestSuite_JavaKafkaLargeBuffer(t *testing.T) {
+	compose, err := docker.ComposeSuite("docker-compose-java-kafka-400-lb.yml", path.Join(pathOutput, "test-suite-java-kafka-lb.log"))
+	compose.Env = append(compose.Env, `BEYLA_OPEN_PORT=8080`, `BEYLA_EXECUTABLE_PATH=`, `TEST_SERVICE_PORTS=8381:8080`)
+	require.NoError(t, err)
+	require.NoError(t, compose.Up())
+	t.Run("Java Kafka 4.0.0 large buffer tests", testJavaKafkaLargeBuffer)
+	require.NoError(t, compose.Close())
+}
+
 func TestSuite_PythonRedis(t *testing.T) {
 	compose, err := docker.ComposeSuite("docker-compose-python-redis.yml", path.Join(pathOutput, "test-suite-python-redis.log"))
 	compose.Env = append(compose.Env, `BEYLA_OPEN_PORT=8080`, `BEYLA_EXECUTABLE_NAME=`, `TEST_SERVICE_PORTS=8381:8080`)

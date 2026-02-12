69ab81458886858e682d24e961dd941de635d957
Stephen Lang <stephen.lang@grafana.com>
2026-02-12 16:35:58 +0000

    chore: remove internal/test/cmd

--- internal/test/cmd/pingserver/debug.sh
+++
@@ -1,1 +0,0 @@
-wrk -R1 -d60s -c1 -t1 --latency http://localhost:8090/smoke
--- internal/test/cmd/pingserver/driver.sh
+++
@@ -1,3 +0,0 @@
-RATE=${1:-100}
-echo Running benchmark with request rate of ${RATE} QPS
-wrk -R${RATE} -d60s -c10 -t10 --latency http://localhost:3090/greeting

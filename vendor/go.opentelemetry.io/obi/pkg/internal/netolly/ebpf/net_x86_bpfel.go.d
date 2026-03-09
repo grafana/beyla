71af5c5ea2d8e037d301ad5060784506ab6e4f75
Nikola Grcevski <grcevski@gmail.com>
2026-03-09 11:11:14 -0400

    vendor obi

---
+++ vendor/go.opentelemetry.io/obi/pkg/internal/netolly/ebpf/net_x86_bpfel.go.d
@@ -0,0 +1,42 @@
+pkg/internal/netolly/ebpf/net_x86_bpfel.go: \
+ bpf/netolly/flows.c \
+ bpf/bpfcore/vmlinux.h \
+ bpf/bpfcore/vmlinux_amd64.h \
+ bpf/bpfcore/bpf_helpers.h \
+ bpf/bpfcore/bpf_helper_defs.h \
+ bpf/bpfcore/bpf_endian.h \
+ bpf/logger/bpf_dbg.h \
+ bpf/bpfcore/bpf_core_read.h \
+ bpf/bpfcore/bpf_tracing.h \
+ bpf/common/globals.h \
+ bpf/common/pin_internal.h \
+ bpf/netolly/flows_common.h \
+ bpf/netolly/flow.h \
+ bpf/common/tc_act.h
+
+bpf/bpfcore/vmlinux.h:
+
+bpf/bpfcore/vmlinux_amd64.h:
+
+bpf/bpfcore/bpf_helpers.h:
+
+bpf/bpfcore/bpf_helper_defs.h:
+
+bpf/bpfcore/bpf_endian.h:
+
+bpf/logger/bpf_dbg.h:
+
+bpf/bpfcore/bpf_core_read.h:
+
+bpf/bpfcore/bpf_tracing.h:
+
+bpf/common/globals.h:
+
+bpf/common/pin_internal.h:
+
+bpf/netolly/flows_common.h:
+
+bpf/netolly/flow.h:
+
+bpf/common/tc_act.h:
+

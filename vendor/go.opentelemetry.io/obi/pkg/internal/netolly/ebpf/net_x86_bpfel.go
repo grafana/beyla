48a617387cbd220b38256828f623634f7eb7936f
Mario Macias <mario.macias@grafana.com>
2026-03-12 10:33:25 +0100

    Update OBI to latest

--- vendor/go.opentelemetry.io/obi/pkg/internal/netolly/ebpf/net_x86_bpfel.go
+++ vendor/go.opentelemetry.io/obi/pkg/internal/netolly/ebpf/net_x86_bpfel.go
@@ -147,6 +147,7 @@ type NetVariableSpecs struct {
 	G_bpfLoopEnabled        *ebpf.VariableSpec `ebpf:"g_bpf_loop_enabled"`
 	G_bpfTraceparentEnabled *ebpf.VariableSpec `ebpf:"g_bpf_traceparent_enabled"`
 	Ip4in6                  *ebpf.VariableSpec `ebpf:"ip4in6"`
+	PortGuessing            *ebpf.VariableSpec `ebpf:"port_guessing"`
 	Sampling                *ebpf.VariableSpec `ebpf:"sampling"`
 	TraceMessages           *ebpf.VariableSpec `ebpf:"trace_messages"`
 	UnusedFlowId            *ebpf.VariableSpec `ebpf:"unused_flow_id"`
@@ -200,6 +201,7 @@ type NetVariables struct {
 	G_bpfLoopEnabled        *ebpf.Variable `ebpf:"g_bpf_loop_enabled"`
 	G_bpfTraceparentEnabled *ebpf.Variable `ebpf:"g_bpf_traceparent_enabled"`
 	Ip4in6                  *ebpf.Variable `ebpf:"ip4in6"`
+	PortGuessing            *ebpf.Variable `ebpf:"port_guessing"`
 	Sampling                *ebpf.Variable `ebpf:"sampling"`
 	TraceMessages           *ebpf.Variable `ebpf:"trace_messages"`
 	UnusedFlowId            *ebpf.Variable `ebpf:"unused_flow_id"`

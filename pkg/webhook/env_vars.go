4a13caa457ed38701a47015fc30860f674107608
Manas Srivastava <mastermanas805@gmail.com>
2026-04-29 19:53:17 +0530

    feat(webhook): inject k8s.pod.ip resource attribute via downward API

--- pkg/webhook/env_vars.go
+++ pkg/webhook/env_vars.go
@@ -120,6 +120,8 @@ func (pm *PodMutator) setResourceAttributes(meta *metav1.ObjectMeta, container *
 	// node name has to be added to extra attributes as there is no dedicated OTEL_INJECTOR_* variable
 	extraResAttrs[semconv.K8SNodeNameKey] =
 		setEnvVarFromFieldPath(container, envOtelK8sNodeName, "spec.nodeName")
+	extraResAttrs[attribute.Key("k8s.pod.ip")] =
+		setEnvVarFromFieldPath(container, envOtelK8sPodIP, "status.podIP")
 
 	if cfg.AddK8sUIDAttributes {
 		setEnvVarFromFieldPath(container, envInjectorOtelK8sPodUID, "metadata.uid")

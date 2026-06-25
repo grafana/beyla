7ba092e0f1db16865b3be12322c4b75dd4570229
Nikola Grcevski <grcevski@gmail.com>
2026-06-25 17:23:06 -0400

    fix posgres detection in go

--- vendor/go.opentelemetry.io/obi/pkg/ebpf/common/spanner.go
+++ vendor/go.opentelemetry.io/obi/pkg/ebpf/common/spanner.go
@@ -127,8 +127,23 @@ func SQLRequestTraceToSpan(trace *SQLRequestTrace) request.Span {
 		hostname = hostname[:idx]
 	}
 
+	subType := trace.Subtype
+
+	// if we didn't detect the type in Go, try heuristic detect
+	if subType == uint8(request.DBGeneric) {
+		switch hostPort {
+		case 5432:
+			subType = uint8(request.DBPostgres)
+		case 3306:
+			subType = uint8(request.DBMySQL)
+		case 1434:
+			subType = uint8(request.DBMSSQL)
+		}
+	}
+
 	return request.Span{
 		Type:          request.EventType(trace.Type),
+		SubType:       int(subType),
 		Method:        method,
 		Path:          path,
 		Peer:          peer,
